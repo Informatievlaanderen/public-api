124cbaa03f7109cd4b8ef9a4e8aab0e1673c40d5
David Cumps <david@cumps.be>
2019-04-17 21:45:00 +0200

    feat: use proper logging for aws fargate

--- src/Public.Api/Infrastructure/Program.cs
+++ src/Public.Api/Infrastructure/Program.cs
@@ -1,24 +1,37 @@
 namespace Public.Api.Infrastructure
 {
-    using System;
-    using System.Security.Cryptography.X509Certificates;
     using Be.Vlaanderen.Basisregisters.Api;
     using Microsoft.AspNetCore.Hosting;
 
     public class Program
     {
-        private static readonly Tuple<string, string> DevelopmentCertificate = new Tuple<string, string>(
-            "api.dev.gemeente.basisregisters.vlaanderen.be.pfx",
-            "gemeenteregister!");
+        private static readonly DevelopmentCertificate DevelopmentCertificate =
+            new DevelopmentCertificate(
+                "api.dev.gemeente.basisregisters.vlaanderen.be.pfx",
+                "gemeenteregister!");
 
         public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
             => new WebHostBuilder()
                 .UseDefaultForApi<Startup>(
-                    httpPort: 2080,
-                    httpsPort: 2443,
-                    httpsCertificate: () => new X509Certificate2(DevelopmentCertificate.Item1, DevelopmentCertificate.Item2),
-                    commandLineArgs: args);
+                    new ProgramOptions
+                    {
+                        Hosting =
+                        {
+                            HttpPort = 2080,
+                            HttpsPort = 2443,
+                            HttpsCertificate = DevelopmentCertificate.ToCertificate,
+                        },
+                        Logging =
+                        {
+                            WriteTextToConsole = false,
+                            WriteJsonToConsole = false
+                        },
+                        Runtime =
+                        {
+                            CommandLineArgs = args
+                        }
+                    });
     }
 }
