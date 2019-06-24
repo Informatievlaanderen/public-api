1f08f09db8486921073cec9dc509924d8c3c7c43
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-06-24 13:16:12 +0200

    feat: add address back to public api

--- src/Public.Api/Infrastructure/Configuration/AddressOptions.cs
+++ src/Public.Api/Infrastructure/Configuration/AddressOptions.cs
@@ -1,9 +1,9 @@
-//namespace Public.Api.Infrastructure.Configuration
-//{
-//    using AddressRegistry.Api.Legacy.Infrastructure.Options;
+namespace Public.Api.Infrastructure.Configuration
+{
+    using AddressRegistry.Api.Legacy.Infrastructure.Options;
 
-//    public class AddressOptions : ResponseOptions, IRegistryOptions
-//    {
-//        public SyndicationOptions Syndication { get; set; }
-//    }
-//}
+    public class AddressOptions : ResponseOptions, IRegistryOptions
+    {
+        public SyndicationOptions Syndication { get; set; }
+    }
+}
