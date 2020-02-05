1ee293012e4500af8a81b03d330787b3bdd51c5d
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-02-05 15:30:12 +0100

    fix: fields with no errors wont show in bad request + update examples

--- src/Public.Api/Infrastructure/ModelStateProblemDetails.cs
+++ src/Public.Api/Infrastructure/ModelStateProblemDetails.cs
@@ -1,32 +1,21 @@
 namespace Public.Api.Infrastructure
 {
-    using System.Collections.Generic;
     using System.Linq;
     using System.Runtime.Serialization;
     using Be.Vlaanderen.Basisregisters.BasicApiProblem;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc.ModelBinding;
-    using Newtonsoft.Json;
 
     /// <summary>
     /// Implementation of Problem Details for HTTP APIs https://tools.ietf.org/html/rfc7807 with additional Validation Errors
     /// </summary>
-    public class ModelStateProblemDetails : StatusCodeProblemDetails
+    [DataContract(Name = "ModelStateProblemDetails", Namespace = "")]
+    public class ModelStateProblemDetails : ValidationProblemDetails
     {
-        /// <summary>Validatie fouten.</summary>
-        [JsonProperty("validationErrors", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        [DataMember(Name = "validationErrors", Order = 600, EmitDefaultValue = false)]
-        public Dictionary<string, string[]> ValidationErrors { get; set; }
-
         // Here to make DataContractSerializer happy
-        public ModelStateProblemDetails(ModelStateDictionary modelState) : base(StatusCodes.Status400BadRequest)
+        public ModelStateProblemDetails(ModelStateDictionary modelState)
         {
-            Title = "Er heeft zich een fout voorgedaan!";
-            Detail = "Validatie mislukt.";
-            ProblemInstanceUri = GetProblemNumber();
-            ProblemTypeUri = "urn:base-registries:validation".ToLowerInvariant();
-
             ValidationErrors = modelState
+                .Where(x => x.Value.Errors.Any())
                 .ToDictionary(
                     x => x.Key,
                     x => x.Value.Errors.Select(error => error.ErrorMessage).ToArray());
