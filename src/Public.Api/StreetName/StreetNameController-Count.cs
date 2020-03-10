9f05e9258effdb4c82024333362de490a8da4a40
David Cumps <david@cumps.be>
2020-03-10 15:27:26 +0100

    fix: ignore streetname in docs

--- src/Public.Api/StreetName/StreetNameController-Count.cs
+++ src/Public.Api/StreetName/StreetNameController-Count.cs
@@ -36,6 +36,7 @@ namespace Public.Api.StreetName
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         [HttpGet("straatnamen/totaal-aantal")]
+        [ApiExplorerSettings(IgnoreApi = true)]
         [ProducesResponseType(typeof(TotaalAantalResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
