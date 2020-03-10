0050ae026c9dd7faa9f8c333660223964975f6e9
David Cumps <david@cumps.be>
2020-03-10 15:25:21 +0100

    fix: ignore building unit count in docs

--- src/Public.Api/BuildingUnit/BuildingUnitController-Count.cs
+++ src/Public.Api/BuildingUnit/BuildingUnitController-Count.cs
@@ -33,6 +33,7 @@ namespace Public.Api.BuildingUnit
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         [HttpGet("gebouweenheden/totaal-aantal")]
+        [ApiExplorerSettings(IgnoreApi = true)]
         [ProducesResponseType(typeof(TotaalAantalResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
