c7a8de307b37d380cb9ec77b2a3c90a7d8b46754
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-03-10 14:50:09 +0100

    feat: add building unit totaal aantal

---
+++ src/Public.Api/BuildingUnit/BuildingUnitController-Count.cs
@@ -0,0 +1,115 @@
+namespace Public.Api.BuildingUnit
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.ETag;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.GrAr.Legacy;
+    using BuildingRegistry.Api.Legacy.BuildingUnit.Query;
+    using BuildingRegistry.Api.Legacy.Infrastructure;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Marvin.Cache.Headers;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using Newtonsoft.Json.Converters;
+    using RestSharp;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class BuildingUnitController
+    {
+        /// <summary>
+        /// Vraag een totaal aantal gebouweenheden op.
+        /// </summary>
+        /// <param name="adresObjectId">Optionele objectidentificator van het gekoppelde adres.</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als de opvraging van het totaal aantal gebouweenheden gelukt is.</response>
+        /// <response code="304">Als de lijst niet gewijzigd is ten opzicht van uw verzoek.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("gebouweenheden/totaal-aantal")]
+        [ProducesResponseType(typeof(List<TotaalAantalResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(TotalCountResponseExample), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultCountCaching, NoStore = true, NoTransform = true)]
+        public async Task<IActionResult> CountBuildingUnits(
+            [FromQuery] int? adresObjectId,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+            => await CountBuildingUnitsWithFormat(
+                null,
+                adresObjectId,
+                actionContextAccessor,
+                ifNoneMatch,
+                cancellationToken);
+
+        /// <summary>
+        /// Vraag een totaal aantal gebouweenheden op.
+        /// </summary>
+        /// <param name="format">Gewenste formaat: json of xml.</param>
+        /// <param name="adresObjectId">Optionele objectidentificator van het gekoppelde adres.</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als de opvraging van het totaal aantal gebouweenheden gelukt is.</response>
+        /// <response code="304">Als de lijst niet gewijzigd is ten opzicht van uw verzoek.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("gebouweenheden/totaal-aantal.{format}")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [ProducesResponseType(typeof(List<TotaalAantalResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(TotalCountResponseExample), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultCountCaching, NoStore = true, NoTransform = true)]
+        public async Task<IActionResult> CountBuildingUnitsWithFormat(
+            [FromRoute] string format,
+            [FromQuery] int? adresObjectId,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+        {
+            format = DetermineAndSetFormat(format, actionContextAccessor, Request);
+
+            IRestRequest BackendRequest() => CreateBackendCountRequest(adresObjectId);
+
+            return new BackendResponseResult(await GetFromBackendAsync(format, BackendRequest, Request.GetTypedHeaders(), CreateDefaultHandleBadRequest(), cancellationToken));
+        }
+
+        private static IRestRequest CreateBackendCountRequest(int? addressId)
+        {
+            var filter = new BuildingUnitFilter
+            {
+                AddressPersistentLocalId = addressId?.ToString()
+            };
+
+            return new RestRequest("gebouweenheden/totaal-aantal")
+                .AddFiltering(filter);
+        }
+    }
+}
