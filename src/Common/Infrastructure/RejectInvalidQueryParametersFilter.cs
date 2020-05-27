bbcf1e8c2f31136554aba52d11a159bc2d8b0e47
David Cumps <david@cumps.be>
2020-05-27 18:17:38 +0200

    fix: point to docs url for valid parameters

--- src/Common/Infrastructure/RejectInvalidQueryParametersFilter.cs
+++ src/Common/Infrastructure/RejectInvalidQueryParametersFilter.cs
@@ -5,15 +5,23 @@ namespace Common.Infrastructure
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
 
     public class RejectInvalidQueryParametersFilterAttribute : Attribute, IResourceFilter
     {
         public void OnResourceExecuting(ResourceExecutingContext context)
         {
+            var configuration = context.HttpContext.RequestServices.GetService<IConfiguration>();
+
             if (context.HttpContext.Request.Query.Keys.Any(x => x.Contains(".")))
                 throw new ApiException(
-                    "Ongeldige parameters. Het gebruik van een prefix bij een parameter is niet geldig.",
+                    $"Ongeldige parameters. Het gebruik van een prefix bij een parameter is niet geldig. Bekijk {configuration["DocsUrl"]} voor een overzicht van geldige parameters.",
                     StatusCodes.Status400BadRequest);
         }
+
+        public void OnResourceExecuted(ResourceExecutedContext context) { }
+
     }
 }
