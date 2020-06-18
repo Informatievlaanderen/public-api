23e4401beb66f2e240c4633b6201c6becddcd408
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-06-18 08:23:06 +0200

    feat: add crab gebouwen endpoint

---
+++ src/Public.Api/CrabBuilding/CrabBuildingController-List.cs
@@ -0,0 +1,139 @@
+namespace Public.Api.CrabBuilding
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.ETag;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.GrAr.Legacy;
+    using BuildingRegistry.Api.Legacy.Building.Query;
+    using BuildingRegistry.Api.Legacy.Building.Responses;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Infrastructure.Configuration;
+    using Marvin.Cache.Headers;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using Microsoft.Extensions.Options;
+    using Newtonsoft.Json.Converters;
+    using RestSharp;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class CrabBuildingController
+    {
+        /// <summary>
+        /// Vraag een lijst met crabgebouwen op die voldoen aan de filter parameters.
+        /// </summary>
+        /// <param name="terreinObjectId">Filter op de crab terrein object id van het gebouw (exact).</param>
+        /// <param name="identificatorTerreinObject">Filter op het crab identificator terreinobject van het gebouw (exact).</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="responseOptions"></param>
+        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als de opvraging van een lijst met crab gebouwen gelukt is.</response>
+        /// <response code="304">Als de lijst niet gewijzigd is ten opzicht van uw verzoek.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("crabgebouwen")]
+        [ProducesResponseType(typeof(List<BuildingCrabMappingResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(BuildingCrabMappingResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultListCaching, NoStore = true, NoTransform = true)]
+        public async Task<IActionResult> ListCrabBuildings(
+            [FromQuery] int? terreinObjectId,
+            [FromQuery] string identificatorTerreinObject,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromServices] IOptions<AddressOptions> responseOptions,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+            => await ListCrabBuildingsWithFormat(
+                null,
+                terreinObjectId,
+                identificatorTerreinObject,
+                actionContextAccessor,
+                responseOptions,
+                ifNoneMatch,
+                cancellationToken);
+
+        /// <summary>
+        /// Vraag een lijst met crabgebouwen op die voldoen aan de filter parameters.
+        /// </summary>
+        /// <param name="terreinObjectId">Filter op de crab terrein object id van het gebouw (exact).</param>
+        /// <param name="identificatorTerreinObject">Filter op het crab identificator terreinobject van het gebouw (exact).</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="responseOptions"></param>
+        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als de opvraging van een lijst met crab gebouwen gelukt is.</response>
+        /// <response code="304">Als de lijst niet gewijzigd is ten opzicht van uw verzoek.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("crabgebouwen.{format}")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [ProducesResponseType(typeof(List<BuildingCrabMappingResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(BuildingCrabMappingResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples), jsonConverter: typeof(StringEnumConverter))]
+        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultListCaching, NoStore = true, NoTransform = true)]
+        public async Task<IActionResult> ListCrabBuildingsWithFormat(
+            [FromRoute] string format,
+            [FromQuery] int? terreinObjectId,
+            [FromQuery] string identificatorTerreinObject,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromServices] IOptions<AddressOptions> responseOptions,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+        {
+            format = DetermineAndSetFormat(format, actionContextAccessor, Request);
+
+            const Taal taal = Taal.NL;
+
+            IRestRequest BackendRequest() => CreateBackendListRequest(
+               terreinObjectId,
+               identificatorTerreinObject);
+
+            var cacheKey = CreateCacheKeyForRequestQuery($"legacy/crabgebouwen-list:{taal}");
+
+            var value = await (CacheToggle.FeatureEnabled
+                ? GetFromCacheThenFromBackendAsync(format, BackendRequest, cacheKey, Request.GetTypedHeaders(), CreateDefaultHandleBadRequest(), cancellationToken)
+                : GetFromBackendAsync(format, BackendRequest, Request.GetTypedHeaders(), CreateDefaultHandleBadRequest(), cancellationToken));
+
+            return BackendListResponseResult.Create(value, Request.Query, string.Empty);
+        }
+
+        private static IRestRequest CreateBackendListRequest(
+            int? terrainObjectId,
+            string identificatorTerrainObject)
+        {
+            var filter = new BuildingCrabMappingFilter
+            {
+                TerrainObjectId = terrainObjectId,
+                IdentifierTerrainObject = identificatorTerrainObject
+            };
+
+            return new RestRequest("crabgebouwen")
+                .AddFiltering(filter);
+        }
+    }
+}
