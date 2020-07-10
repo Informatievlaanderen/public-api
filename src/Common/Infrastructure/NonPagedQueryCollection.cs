32a25065ef51e97d301a83f1f51cdc6b2d4c29c5
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-07-10 15:06:30 +0200

    fix: clean query parameters GRAR-1497

--- src/Common/Infrastructure/NonPagedQueryCollection.cs
+++ src/Common/Infrastructure/NonPagedQueryCollection.cs
@@ -3,6 +3,7 @@ namespace Common.Infrastructure
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Primitives;
 
@@ -15,18 +16,24 @@ namespace Common.Infrastructure
 
         private static readonly IEnumerable<string> PageQueryParameters = new[] {"limit", "offset"};
 
+        private static readonly Regex CleanXmlKeyRegex = new Regex(@"^(amp;)+", RegexOptions.IgnoreCase);
+
         private static Dictionary<string, StringValues> Filter(IEnumerable<KeyValuePair<string, StringValues>> query)
         {
             if (query == null)
                 return new Dictionary<string, StringValues>();
 
-            bool IsNotPageParameter(KeyValuePair<string, StringValues> keyValuePair)
-                => PageQueryParameters.Contains(keyValuePair.Key, StringComparer.InvariantCultureIgnoreCase) == false;
-
             return query
                 .Where(queryParameter => queryParameter.Value.Count > 0)
+                .Select(CleanParameterKeys)
                 .Where(IsNotPageParameter)
                 .ToDictionary(queryParameter => queryParameter.Key, queryParameter => queryParameter.Value);
         }
+
+        private static bool IsNotPageParameter(KeyValuePair<string, StringValues> keyValuePair)
+            => PageQueryParameters.Contains(keyValuePair.Key, StringComparer.InvariantCultureIgnoreCase) == false;
+
+        private static KeyValuePair<string, StringValues> CleanParameterKeys(KeyValuePair<string, StringValues> parameter)
+            => new KeyValuePair<string, StringValues>(CleanXmlKeyRegex.Replace(parameter.Key, string.Empty), parameter.Value);
     }
 }
