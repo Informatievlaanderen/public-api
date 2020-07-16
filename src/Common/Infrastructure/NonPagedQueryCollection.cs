0033fedaf754c9b6cf3a2ad3427d6d2d37c810b8
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-07-16 11:32:12 +0200

    fix: clean xlm escpapes from querystring GRAR-1497

--- src/Common/Infrastructure/NonPagedQueryCollection.cs
+++ src/Common/Infrastructure/NonPagedQueryCollection.cs
@@ -16,8 +16,6 @@ namespace Common.Infrastructure
 
         private static readonly IEnumerable<string> PageQueryParameters = new[] {"limit", "offset"};
 
-        private static readonly Regex CleanXmlKeyRegex = new Regex(@"^(amp;)+", RegexOptions.IgnoreCase);
-
         private static Dictionary<string, StringValues> Filter(IEnumerable<KeyValuePair<string, StringValues>> query)
         {
             if (query == null)
@@ -25,15 +23,11 @@ namespace Common.Infrastructure
 
             return query
                 .Where(queryParameter => queryParameter.Value.Count > 0)
-                .Select(CleanParameterKeys)
                 .Where(IsNotPageParameter)
                 .ToDictionary(queryParameter => queryParameter.Key, queryParameter => queryParameter.Value);
         }
 
         private static bool IsNotPageParameter(KeyValuePair<string, StringValues> keyValuePair)
             => PageQueryParameters.Contains(keyValuePair.Key, StringComparer.InvariantCultureIgnoreCase) == false;
-
-        private static KeyValuePair<string, StringValues> CleanParameterKeys(KeyValuePair<string, StringValues> parameter)
-            => new KeyValuePair<string, StringValues>(CleanXmlKeyRegex.Replace(parameter.Key, string.Empty), parameter.Value);
     }
 }
