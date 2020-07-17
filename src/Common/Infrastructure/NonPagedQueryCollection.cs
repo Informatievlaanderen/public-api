aba6701a614a8408f398ccbe77a5a42652d422e9
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-07-17 17:25:26 +0200

    fix: remove empty parameters from next url GRAR-1497

--- src/Common/Infrastructure/NonPagedQueryCollection.cs
+++ src/Common/Infrastructure/NonPagedQueryCollection.cs
@@ -22,12 +22,15 @@ namespace Common.Infrastructure
                 return new Dictionary<string, StringValues>();
 
             return query
-                .Where(queryParameter => queryParameter.Value.Count > 0)
                 .Where(IsNotPageParameter)
+                .Where(HasValue)
                 .ToDictionary(queryParameter => queryParameter.Key, queryParameter => queryParameter.Value);
         }
 
         private static bool IsNotPageParameter(KeyValuePair<string, StringValues> keyValuePair)
             => PageQueryParameters.Contains(keyValuePair.Key, StringComparer.InvariantCultureIgnoreCase) == false;
+
+        private static bool HasValue(KeyValuePair<string, StringValues> parameter)
+            => parameter.Value.Count > 0 && !string.IsNullOrWhiteSpace(parameter.Value.ToString());
     }
 }
