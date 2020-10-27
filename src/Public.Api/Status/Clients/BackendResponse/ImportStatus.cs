a311ec5bb05f2d3f77971adf51c3d44c32f25c16
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-10-27 18:06:13 +0100

    fix: decoupled from ImportStatus from grar.import

--- src/Public.Api/Status/Clients/BackendResponse/ImportStatus.cs
+++ src/Public.Api/Status/Clients/BackendResponse/ImportStatus.cs
@@ -1,6 +1,20 @@
 namespace Public.Api.Status.Clients.BackendResponse
 {
+    using System;
     using System.Collections.Generic;
 
-    public class ImportStatusList : List<Be.Vlaanderen.Basisregisters.GrAr.Import.Processing.Api.Messages.ImportStatus> { }
+    public class ImportStatusList : List<ImportStatus> { }
+
+    public class ImportStatus
+    {
+        public string Name { get; set; }
+        public ImportStatusBatchScope LastCompletedBatch { get; set; }
+        public ImportStatusBatchScope CurrentBatch { get; set; }
+    }
+
+    public class ImportStatusBatchScope
+    {
+        public DateTimeOffset From { get; set; }
+        public DateTimeOffset Until { get; set; }
+    }
 }
