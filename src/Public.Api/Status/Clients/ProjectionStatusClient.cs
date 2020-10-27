b15ccecffa72ae613055bdafaae66a3b9fb8e4bf
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-10-27 17:21:04 +0100

    feat: entend projection status to show projection progress

--- src/Public.Api/Status/Clients/ProjectionStatusClient.cs
+++ src/Public.Api/Status/Clients/ProjectionStatusClient.cs
@@ -1,12 +1,12 @@
 namespace Public.Api.Status.Clients
 {
-    using System.Collections.Generic;
     using System.Linq;
+    using BackendResponse;
     using Common.Infrastructure;
     using Responses;
     using RestSharp;
 
-    public class ProjectionStatusClient : BaseStatusClient<IEnumerable<RegistryProjectionStatus>, IEnumerable<ProjectionStatusClient.ProjectionStatus>>
+    public class ProjectionStatusClient : BaseStatusClient<RegistryProjectionStatusResponse, ProjectionsStatusList>
     {
         public ProjectionStatusClient(string registry, TraceRestClient restClient)
             : base(registry, restClient) { }
@@ -14,18 +14,19 @@ namespace Public.Api.Status.Clients
         protected override IRestRequest CreateStatusRequest()
             => new RestRequest("projections");
 
-        protected override IEnumerable<RegistryProjectionStatus> Map(IEnumerable<ProjectionStatusClient.ProjectionStatus> response)
-            => response.Select(status =>
-                new RegistryProjectionStatus
-                {
-                    Name = status.Name,
-                    State = status.State,
-                });
-
-        public class ProjectionStatus
-        {
-            public string Name { get; set; }
-            public string State { get; set; }
-        }
+        protected override RegistryProjectionStatusResponse Map(ProjectionsStatusList response)
+            => new RegistryProjectionStatusResponse
+            {
+                StreamPosition = response.StreamPosition,
+                Projections = response
+                    .Projections
+                    .Select(status =>
+                        new RegistryProjectionStatus
+                        {
+                            Name = status.ProjectionName,
+                            State = status.ProjectionState,
+                            CurrentPosition = status.CurrentPosition
+                        })
+            };
     }
 }
