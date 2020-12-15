12ab97d1f410c96074b46d29128524d3ccd31df7
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-12-15 10:31:17 +0100

    feat: add total count crab endpoints GRAR-1258 GRAR-1238

---
+++ src/Public.Api/CrabSubaddress/CrabSubaddressController-Count.cs
@@ -0,0 +1,107 @@
+namespace Public.Api.CrabSubaddress
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AddressRegistry.Api.Legacy.Address.Responses;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.Api.LastObservedPosition;
+    using Be.Vlaanderen.Basisregisters.GrAr.Legacy;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Marvin.Cache.Headers;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using RestSharp;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class CrabSubaddressController
+    {
+        /// <summary>
+        /// Vraag het totaal aantal crab subadressen op.
+        /// </summary>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als de opvraging van het totaal aantal crab subadressen gelukt is.</response>
+        /// <response code="304">Als het aantal niet gewijzigd is ten opzicht van uw verzoek.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("crabsubadressen/totaal-aantal", Name = nameof(CountSubaddresses))]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [ProducesResponseType(typeof(TotaalAantalResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(TotalCountResponseExample))]
+        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultCountCaching, NoStore = true, NoTransform = true)]
+        public async Task<IActionResult> CountSubaddresses(
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+            => await CountSubaddressesWithFormat(
+                null,
+                actionContextAccessor,
+                ifNoneMatch,
+                cancellationToken);
+
+        /// <summary>
+        /// Vraag het totaal aantal crab subadressen op.
+        /// </summary>
+        /// <param name="format">Gewenste formaat: json of xml.</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als de opvraging van het totaal aantal crab subadressen gelukt is.</response>
+        /// <response code="304">Als het aantal niet gewijzigd is ten opzicht van uw verzoek.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("crabsubadressen/totaal-aantal.{format}", Name = nameof(CountSubaddressesWithFormat))]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [ProducesResponseType(typeof(TotaalAantalResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(TotalCountResponseExample))]
+        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultCountCaching, NoStore = true, NoTransform = true)]
+        public async Task<IActionResult> CountSubaddressesWithFormat(
+            [FromRoute] string format,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+        {
+            var contentFormat = DetermineFormat(format, actionContextAccessor.ActionContext);
+
+            IRestRequest BackendRequest() => CreateBackendCountRequest();
+
+            return new BackendResponseResult(
+                await GetFromBackendAsync(
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    CreateDefaultHandleBadRequest(),
+                    cancellationToken));
+        }
+
+        private static IRestRequest CreateBackendCountRequest()
+        {
+            return new RestRequest("crabsubadressen/totaal-aantal");
+        }
+    }
+}
