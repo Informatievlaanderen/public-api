2077ba83f5aaf576ce5f6e5d06f0e045cca5ffbe
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2021-01-25 13:42:48 +0100

    refactor: removed .format from feeds endpoints GRAR-1317

--- src/Public.Api/Feeds/Addresses.cs
+++ src/Public.Api/Feeds/Addresses.cs
@@ -22,59 +22,8 @@ namespace Public.Api.Feeds
     public partial class FeedController
     {
         /// <summary>
-        /// Vraag een lijst met wijzigingen op adressen op in het Atom formaat.
-        /// </summary>
-        /// <param name="actionContextAccessor"></param>
-        /// <param name="restClients"></param>
-        /// <param name="responseOptions"></param>
-        /// <param name="from">Eventidentificator (volgnummer) vanaf waar de feed moet gestart of hernomen worden.</param>
-        /// <param name="limit">Maximaal aantal instanties dat teruggegeven wordt.</param>
-        /// <param name="embed">Keuze welke info in het <![CDATA[&lt;Content&gt;]]>-gedeelte van de output moet zitten. ("event", "object", "event,object")</param>
-        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Als de opvraging van een lijst met wijzigingen op adressen gelukt is.</response>
-        /// <response code="304">Als de lijst niet gewijzigd is ten opzicht van uw verzoek.</response>
-        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
-        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
-        /// <response code="500">Als er een interne fout is opgetreden.</response>
-        [HttpGet("adressen", Name = nameof(GetAddressesFeed))]
-        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
-        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(AddressSyndicationResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
-        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
-        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultFeedCaching, NoStore = true, NoTransform = true)]
-        public async Task<IActionResult> GetAddressesFeed(
-            [FromServices] IActionContextAccessor actionContextAccessor,
-            [FromServices] IIndex<string, Lazy<IRestClient>> restClients,
-            [FromServices] IOptions<AddressOptions> responseOptions,
-            [FromQuery] long? from,
-            [FromQuery] int? limit,
-            [FromQuery] string embed,
-            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
-            CancellationToken cancellationToken = default)
-            => await GetAddressesFeedWithFormat(
-                null,
-                actionContextAccessor,
-                restClients,
-                responseOptions,
-                from,
-                limit,
-                embed,
-                ifNoneMatch,
-                cancellationToken);
-
-        /// <summary>
         /// Vraag een lijst met wijzigingen op adressen op in het XML of Atom formaat.
         /// </summary>
-        /// <param name="format">Gewenste formaat: adressen.xml of adressen.atom</param>
         /// <param name="actionContextAccessor"></param>
         /// <param name="restClients"></param>
         /// <param name="responseOptions"></param>
@@ -88,8 +37,7 @@ namespace Public.Api.Feeds
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
-        [HttpGet("adressen.{format}", Name = nameof(GetAddressesFeedWithFormat))]
-        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet("adressen", Name = nameof(GetAddressesFeedWithFormat))]
         [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
@@ -104,7 +52,6 @@ namespace Public.Api.Feeds
         [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
         [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultFeedCaching, NoStore = true, NoTransform = true)]
         public async Task<IActionResult> GetAddressesFeedWithFormat(
-            [FromRoute] string format,
             [FromServices] IActionContextAccessor actionContextAccessor,
             [FromServices] IIndex<string, Lazy<IRestClient>> restClients,
             [FromServices] IOptions<AddressOptions> responseOptions,
@@ -114,7 +61,7 @@ namespace Public.Api.Feeds
             [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
             CancellationToken cancellationToken = default)
         {
-            var contentFormat = DetermineFormat(format, actionContextAccessor.ActionContext);
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
 
             IRestRequest BackendRequest() => CreateBackendSyndicationRequest(
                 "adressen",
@@ -129,7 +76,7 @@ namespace Public.Api.Feeds
                 HandleBadRequest,
                 cancellationToken);
 
-            return BackendListResponseResult.Create(value, Request.Query, responseOptions.Value.Syndication.NextUri, contentFormat.UrlExtension);
+            return BackendListResponseResult.Create(value, Request.Query, responseOptions.Value.Syndication.NextUri);
         }
     }
 }
--- src/Public.Api/Feeds/Streetnames.cs
+++ src/Public.Api/Feeds/Streetnames.cs
@@ -22,59 +22,8 @@ namespace Public.Api.Feeds
     public partial class FeedController
     {
         /// <summary>
-        /// Vraag een lijst met wijzigingen op straatnamen op in het Atom formaat.
-        /// </summary>
-        /// <param name="actionContextAccessor"></param>
-        /// <param name="restClients"></param>
-        /// <param name="responseOptions"></param>
-        /// <param name="from">Eventidentificator (volgnummer) vanaf waar de feed moet gestart of hernomen worden.</param>
-        /// <param name="limit">Maximaal aantal instanties dat teruggegeven wordt.</param>
-        /// <param name="embed">Keuze welke info in het <![CDATA[&lt;Content&gt;]]>-gedeelte van de output moet zitten. ("event", "object", "event,object")</param>
-        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Als de opvraging van een lijst met wijzigingen op straatnamen gelukt is.</response>
-        /// <response code="304">Als de lijst niet gewijzigd is ten opzicht van uw verzoek.</response>
-        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
-        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
-        /// <response code="500">Als er een interne fout is opgetreden.</response>
-        [HttpGet("straatnamen", Name = nameof(GetStreetNamesFeed))]
-        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
-        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(StreetNameSyndicationResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
-        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
-        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultFeedCaching, NoStore = true, NoTransform = true)]
-        public async Task<IActionResult> GetStreetNamesFeed(
-            [FromServices] IActionContextAccessor actionContextAccessor,
-            [FromServices] IIndex<string, Lazy<IRestClient>> restClients,
-            [FromServices] IOptions<StreetNameOptions> responseOptions,
-            [FromQuery] long? from,
-            [FromQuery] int? limit,
-            [FromQuery] string embed,
-            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
-            CancellationToken cancellationToken = default)
-            => await GetStreetNamesFeedWithFormat(
-                null,
-                actionContextAccessor,
-                restClients,
-                responseOptions,
-                from,
-                limit,
-                embed,
-                ifNoneMatch,
-                cancellationToken);
-
-        /// <summary>
         /// Vraag een lijst met wijzigingen op straatnamen op in het XML of Atom formaat.
         /// </summary>
-        /// <param name="format">Gewenste formaat: straatnamen.xml of straatnamen.atom</param>
         /// <param name="actionContextAccessor"></param>
         /// <param name="restClients"></param>
         /// <param name="responseOptions"></param>
@@ -88,8 +37,7 @@ namespace Public.Api.Feeds
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
-        [HttpGet("straatnamen.{format}", Name = nameof(GetStreetNamesFeedWithFormat))]
-        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet("straatnamen", Name = nameof(GetStreetNamesFeedWithFormat))]
         [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
@@ -104,7 +52,6 @@ namespace Public.Api.Feeds
         [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
         [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultFeedCaching, NoStore = true, NoTransform = true)]
         public async Task<IActionResult> GetStreetNamesFeedWithFormat(
-            [FromRoute] string format,
             [FromServices] IActionContextAccessor actionContextAccessor,
             [FromServices] IIndex<string, Lazy<IRestClient>> restClients,
             [FromServices] IOptions<StreetNameOptions> responseOptions,
@@ -114,7 +61,7 @@ namespace Public.Api.Feeds
             [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
             CancellationToken cancellationToken = default)
         {
-            var contentFormat = DetermineFormat(format, actionContextAccessor.ActionContext);
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
 
             IRestRequest BackendRequest() => CreateBackendSyndicationRequest(
                 "straatnamen",
@@ -129,7 +76,7 @@ namespace Public.Api.Feeds
                 HandleBadRequest,
                 cancellationToken);
 
-            return BackendListResponseResult.Create(value, Request.Query, responseOptions.Value.Syndication.NextUri, contentFormat.UrlExtension);
+            return BackendListResponseResult.Create(value, Request.Query, responseOptions.Value.Syndication.NextUri);
         }
     }
 }
