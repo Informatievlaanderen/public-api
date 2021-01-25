7d2057b8420efb010df7c4c4f72b70eb2c847426
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2021-01-25 13:58:51 +0100

    refactor: removed .format from postal endpoints GRAR-1317

--- src/Public.Api/PostalCode/PostalCodeController-Count.cs
+++ src/Public.Api/PostalCode/PostalCodeController-Count.cs
@@ -31,47 +31,7 @@ namespace Public.Api.PostalCode
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
-        [HttpGet("postinfo/totaal-aantal", Name = nameof(CountPostalCodes))]
-        [ApiExplorerSettings(IgnoreApi = true)]
-        [ProducesResponseType(typeof(TotaalAantalResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status406NotAcceptable)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de respons.")]
-        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(TotalCountResponseExample))]
-        [SwaggerResponseExample(StatusCodes.Status304NotModified, typeof(NotModifiedResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status406NotAcceptable, typeof(NotAcceptableResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
-        [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
-        [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultCountCaching, NoStore = true, NoTransform = true)]
-        public async Task<IActionResult> CountPostalCodes(
-            [FromQuery] string gemeentenaam,
-            [FromServices] IActionContextAccessor actionContextAccessor,
-            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
-            CancellationToken cancellationToken = default)
-            => await CountPostalCodesWithFormat(
-                null,
-                gemeentenaam,
-                actionContextAccessor,
-                ifNoneMatch,
-                cancellationToken);
-
-        /// <summary>
-        /// Vraag het totaal aantal postinfo over postcodes op.
-        /// </summary>
-        /// <param name="format">Gewenste formaat: json of xml.</param>
-        /// <param name="gemeentenaam">De gerelateerde gemeentenaam van de postcodes (exact).</param>
-        /// <param name="actionContextAccessor"></param>
-        /// <param name="ifNoneMatch">Optionele If-None-Match header met ETag van een vorig verzoek.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Als de opvraging van het totaal aantal postinfo over postcodes gelukt is.</response>
-        /// <response code="304">Als het aantal niet gewijzigd is ten opzicht van uw verzoek.</response>
-        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
-        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
-        /// <response code="500">Als er een interne fout is opgetreden.</response>
-        [HttpGet("postinfo/totaal-aantal.{format}", Name = nameof(CountPostalCodesWithFormat))]
+        [HttpGet("postinfo/totaal-aantal", Name = nameof(CountPostalCodesWithFormat))]
         [ApiExplorerSettings(IgnoreApi = true)]
         [ProducesResponseType(typeof(TotaalAantalResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified)]
@@ -87,13 +47,12 @@ namespace Public.Api.PostalCode
         [HttpCacheValidation(NoCache = true, MustRevalidate = true, ProxyRevalidate = true)]
         [HttpCacheExpiration(CacheLocation = CacheLocation.Private, MaxAge = DefaultCountCaching, NoStore = true, NoTransform = true)]
         public async Task<IActionResult> CountPostalCodesWithFormat(
-            [FromRoute] string format,
             [FromQuery] string gemeentenaam,
             [FromServices] IActionContextAccessor actionContextAccessor,
             [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
             CancellationToken cancellationToken = default)
         {
-            var contentFormat = DetermineFormat(format, actionContextAccessor.ActionContext);
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
 
             IRestRequest BackendRequest() => CreateBackendCountRequest(gemeentenaam);
 
