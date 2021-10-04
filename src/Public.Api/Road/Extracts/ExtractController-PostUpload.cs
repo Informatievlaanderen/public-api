173625ce266eb973d18a88d1dc2417ad8563883d
ChristopheVr <christophe.vroonen@gmail.com>
2021-10-04 15:37:21 +0200

    feat: add roadregistry endpoint to upload extracts

---
+++ src/Public.Api/Road/Extracts/ExtractController-PostUpload.cs
@@ -0,0 +1,44 @@
+namespace Public.Api.Road.Extracts
+{
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    public partial class ExtractController
+    {
+        [HttpPost("wegen/extract/download/{downloadId}/uploads")]
+        public async Task<ActionResult> PostUpload(
+            [FromRoute]string downloadId,
+            [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            IFormFile archive,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await GetFromBackendWithBadRequestAsync(
+                _httpClient,
+                () => CreateBackendUploadRequest(downloadId, archive),
+                CreateDefaultHandleBadRequest(),
+                problemDetailsHelper,
+                cancellationToken
+            );
+
+            return response.ToActionResult();
+        }
+
+        private static HttpRequestMessage CreateBackendUploadRequest(
+            string downloadId,
+            IFormFile archive) => new HttpRequestMessage(HttpMethod.Post, $"download/{downloadId}/uploads")
+        {
+            Content = new StreamContent(archive.OpenReadStream()),
+            Headers =
+            {
+                {HeaderNames.ContentDisposition, archive.ContentDisposition},
+                {HeaderNames.ContentType, archive.ContentType}
+            }
+        };
+    }
+}
