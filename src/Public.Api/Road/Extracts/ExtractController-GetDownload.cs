512eff2e8befaa3282b879a7bed3fd4febd36712
ChristopheVr <christophe.vroonen@gmail.com>
2021-10-04 14:17:17 +0200

    feat: add endpoint for roadregistry extract download request

---
+++ src/Public.Api/Road/Extracts/ExtractController-GetDownload.cs
@@ -0,0 +1,31 @@
+namespace Public.Api.Road.Extracts
+{
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Infrastructure;
+    using Microsoft.AspNetCore.Mvc;
+
+    public partial class ExtractController
+    {
+        [HttpPost("wegen/extract/download/{downloadId}")]
+        public async Task<ActionResult> PostDownloadRequest(
+            [FromRoute]string downloadId,
+            [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await GetFromBackendWithBadRequestAsync(
+                _httpClient,
+                () => CreateBackendDownloadRequest(downloadId),
+                CreateDefaultHandleBadRequest(),
+                problemDetailsHelper,
+                cancellationToken
+            );
+
+            return response.ToActionResult();
+        }
+
+        private static HttpRequestMessage CreateBackendDownloadRequest(string downloadId) => new HttpRequestMessage(HttpMethod.Get, $"download/{downloadId}");
+    }
+}
