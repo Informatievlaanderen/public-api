252747a96214e2e55885b918ed6f5af6c6b29730
Alain Thoen <Alain.Thoen@streamsoftware.be>
2022-02-04 13:35:51 +0100

    fix: added docu foutmeldingen

--- src/Public.Api/Infrastructure/Startup.cs
+++ src/Public.Api/Infrastructure/Startup.cs
@@ -554,6 +554,23 @@ De Basisregisters Vlaanderen API gebruikt [Problem Details for HTTP APIs (RFC780
   ""instance"": ""string""
 }}
 ```
+
+## Mogelijke foutmeldingen
+
+Binnen de aangeboden endpoints zijn er een aantal foutmeldingen die kunnen voorkomen. U moet naar het veld ‘Detail’ kijken voor meer informatie.
+
+Foutmelding | Wanneer                                                           |
+----------- | ----------------------------------------------------------------- |
+304    | Wanneer de request niet gewijzigd is tegenover de vorige opvraging.  |
+400    | Wanneer uw verzoek foutieve data bevat. Bijvoorbeeld:        <br>     -	Wanneer het veld numeriek is, maar er geen numerieke waarde wordt meegegeven,<br>            -	Wanneer bij de request parameter een . wordt meegegeven,<br>            -	Wanneer bij endpoint ‘Crabgebouwen’ er geen parameters worden meegegeven.|
+401    |Wanneer er geen API key in de feed wordt meegegeven. |
+403    |Wanneer het formaat in de URL wordt meegegeven. <br> Wanneer u een API key meegeeft die niet correct is. |
+404    |Wanneer het objectid niet gevonden kan worden. |
+406    |Wanneer het verkeerde formaat wordt meegegeven in de accept header.|
+410    |Wanneer het objectid verwijderd is.|
+500    |Wanneer de response groter is dan 10MB.<br> Wanneer er een interne fout is gebeurd. <br> Wanneer de GRB WFS-service niet kan gecontacteerd worden. |
+
+ 
 ");
 
             if (isFeedsVisibleToggle)
