7b8cf604835b6b60117bf46d70604c7f328dfa2b
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-03-04 15:19:37 +0100

    feat: add x-api-token

--- src/Common/Infrastructure/Controllers/Attributes/ApiKeyAuthAttribute.cs
+++ src/Common/Infrastructure/Controllers/Attributes/ApiKeyAuthAttribute.cs
@@ -2,6 +2,7 @@ namespace Common.Infrastructure.Controllers.Attributes
 {
     using System;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Extensions;
@@ -9,62 +10,117 @@ namespace Common.Infrastructure.Controllers.Attributes
     using Microsoft.AspNetCore.Mvc.Filters;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Primitives;
+    using Newtonsoft.Json;
 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
     {
         private const string ApiKeyHeaderName = "x-api-key";
         private const string ApiKeyQueryName = "apikey";
+        private const string ApiTokenHeaderName = "x-api-token";
 
-        private readonly string _validApiKeySet;
+        private readonly string _requiredAccess;
 
-        public ApiKeyAuthAttribute(string validApiKeySet)
-            => _validApiKeySet = validApiKeySet;
+        public ApiKeyAuthAttribute(string requiredAccess)
+            => _requiredAccess = requiredAccess;
 
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        public Task OnActionExecutionApiKeyAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            void RefuseAccess(HttpContext httpContext, string message)
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialHeaderApiKey)
+                && !context.HttpContext.Request.Query.TryGetValue(ApiKeyQueryName, out var potentialQueryApiKey))
             {
-                if (!(context.Controller is PublicApiController))
-                    return;
-
-                context.SetContentFormatAcceptType();
-
-                throw new ApiException(message, StatusCodes.Status401Unauthorized);
+                RefuseAccess(context, "API key verplicht.");
             }
 
-            var potentialHeaderApiKey = StringValues.Empty;
-            var potentialQueryApiKey = StringValues.Empty;
-
-            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out potentialHeaderApiKey) &&
-                !context.HttpContext.Request.Query.TryGetValue(ApiKeyQueryName, out potentialQueryApiKey))
-                RefuseAccess(context.HttpContext, "API key verplicht.");
-
             var potentialApiKey = string.Empty;
-
             if (!string.IsNullOrWhiteSpace(potentialQueryApiKey))
+            {
                 potentialApiKey = potentialQueryApiKey;
+            }
 
             if (!string.IsNullOrWhiteSpace(potentialHeaderApiKey))
+            {
                 potentialApiKey = potentialHeaderApiKey;
+            }
 
             if (string.IsNullOrWhiteSpace(potentialApiKey))
-                RefuseAccess(context.HttpContext, "API key verplicht.");
+            {
+                RefuseAccess(context, "API key verplicht.");
+            }
 
-            var valiApiKeys = context
+            var validApiKeys = context
                 .HttpContext
                 .RequestServices
                 .GetRequiredService<IConfiguration>()
-                .GetSection($"ApiKeys:{_validApiKeySet}")
+                .GetSection($"ApiKeys:{_requiredAccess}")
                 .GetChildren()
                 .Select(c => c.Value)
                 .ToArray();
 
-            if (!valiApiKeys.Contains(potentialApiKey))
-                RefuseAccess(context.HttpContext, "Ongeldige API key.");
+            if (!validApiKeys.Contains(potentialApiKey))
+            {
+                RefuseAccess(context, "Ongeldige API key.");
+            }
 
-            await next();
+            return next();
+        }
+
+        internal record ApiToken([JsonProperty("clientname")] string ClientName, [JsonProperty("apikey")] string ApiKey, [JsonProperty("metadata")] ApiTokenMetadata Metadata);
+        internal record ApiTokenMetadata([JsonProperty("wraccess")] bool WrAccess, [JsonProperty("syncaccess")] bool SyncAccess);
+
+        public Task OnActionExecutionApiTokenAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiTokenHeaderName, out var potentialHeaderApiTokens))
+            {
+                RefuseAccess(context, "Gelieve een geldige API key op te geven");
+                return next();
+            }
+
+            var potentialHeaderApiToken = potentialHeaderApiTokens.FirstOrDefault();
+            if (potentialHeaderApiToken is null)
+            {
+                RefuseAccess(context, "Ongeldige API key");
+                return next();
+            }
+
+            var bytes = Convert.FromBase64String(potentialHeaderApiToken);
+            var json = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            var apiToken = JsonConvert.DeserializeObject<ApiToken>(json);
+
+            var wrAccess = apiToken?.Metadata.WrAccess;
+            var syncAccess = apiToken?.Metadata.SyncAccess;
+
+            if ((_requiredAccess.Equals("road", StringComparison.InvariantCultureIgnoreCase) && !(wrAccess ?? false))
+            || (_requiredAccess.Equals("sync", StringComparison.InvariantCultureIgnoreCase) && !(syncAccess ?? false)))
+            {
+                RefuseAccess(context, "Geen toegang");
+                return next();
+            }
+
+            return next();
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.HttpContext.Request.Headers.ContainsKey(ApiTokenHeaderName))
+            {
+                await OnActionExecutionApiTokenAsync(context, next);
+                return;
+            }
+
+            await OnActionExecutionApiKeyAsync(context, next);
+        }
+
+        private void RefuseAccess(ActionExecutingContext context, string message)
+        {
+            if (!(context.Controller is PublicApiController))
+            {
+                return;
+            }
+
+            context.SetContentFormatAcceptType();
+
+            throw new ApiException(message, StatusCodes.Status401Unauthorized);
         }
     }
 }
