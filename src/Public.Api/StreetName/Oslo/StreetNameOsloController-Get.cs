cf6fa07eb0a881dcd71c5592dccfcf817250d4c8
Arne Dumarey <dumarey.arne@gmail.com>
2022-03-06 11:35:11 +0100

    fix: also let v2 return etag

--- src/Public.Api/StreetName/Oslo/StreetNameOsloController-Get.cs
+++ src/Public.Api/StreetName/Oslo/StreetNameOsloController-Get.cs
@@ -79,7 +79,7 @@ namespace Public.Api.StreetName.Oslo
                     CreateDefaultHandleBadRequest(),
                     cancellationToken));
 
-            return new BackendResponseResult(value);
+            return new BackendResponseResult(value, BackendResponseResultOptions.ForRead());
         }
 
         private static RestRequest CreateBackendDetailRequest(int streetNameId)
