b19bbc2dc4fd89720bcfbf9498f075c39312b367
pgallik <patrickgallik@gmail.com>
2022-04-14 16:28:28 +0200

    docs: anonymous access text change

--- src/Public.Api/Infrastructure/Startup.cs
+++ src/Public.Api/Infrastructure/Startup.cs
@@ -525,12 +525,12 @@ Iedereen    | {_configuration["BaseUrl"]} |
 
 ### Toegang
 
-De read endpoints zijn anoniem raadpleegbaar, maar er is een beperking aanwezig op het aantal verzoeken dat u tegelijkertijd kan versturen naar deze endpoints. Deze houdt in dat de toepassing maximaal 1 request per seconde toelaat en de burst is maximaal 3 requests. Wanneer u een API key meegeeft dan is er geen beperking aanwezig.
+De read endpoints zijn anoniem raadpleegbaar echter is er een beperking aanwezig op het aantal verzoeken dat u tegelijkertijd kan versturen naar deze endpoints. Wanneer u een API key meegeeft dan zal u meer requests kunnen versturen.
 
 Om dus optimaal gebruik te maken van de endpoints vraagt u best een API key aan. Dit kan door uw gegevens in volgende link achter te laten: [Vraag hier uw API key aan](https://dynamicforms.crmiv.vlaanderen.be/DynamicForms/Page/Show/CfDJ8M4Eu9v84l9JmW3p7WGylS-u2ToCLC5KvqQZmZ4G99X5TBULO4n0LCDpm7870eDUOk90hogqVcE7BCVQf2u_4WlsZ7B8friBrkyuAqmXYpIX_BzvQVVo8eUZyNd-njc33Y-Z-B87y03Y2Jgukp2AN5U93jT1Xv2l0afgvenLD9k0fasSMJkt4uNzKmlr_gILGrOy%2FJSqnRom_MLu0h7sALJ8uNvPywCMsZ1zy5Lal4h63?path=APIKey-aanvraag). U kan deze API key op 2 manieren meegeven:
 
-* Via de header `x-api-key`.
-* In de URL. Bijvoorbeeld: `{baseUrlWithGroupName}/adressen?apikey={{apikey}}` waarbij `{{apikey}}` vervangen wordt door de unieke code van uw API key.
+* Via de header x-api-key.
+* In de URL. Bijvoorbeeld: {baseUrlWithGroupName}/adressen?apikey={{apikey}} waarbij {{apikey}} vervangen wordt door de unieke code van uw API key.
 
 ### V1 vs v2
 
