7ac7356a6fb6dc76cc02e332af56d6f32f4cf0c1
Arne Dumarey <dumarey.arne@gmail.com>
2022-06-20 13:50:31 +0200

    fix: typo

--- src/Public.Api/Building/BackOffice/BuildingBackOfficerController-Realize.cs
+++ src/Public.Api/Building/BackOffice/BuildingBackOfficerController-Realize.cs
@@ -23,7 +23,7 @@ namespace Public.Api.Building.BackOffice
         /// <param name="problemDetailsHelper"></param>
         /// <param name="realizeBuildingToggle"></param>
         /// <param name="cancellationToken"></param>
-        /// <response code="202">Als de aanvraag om het gebouw te realizeren succesvol is.</response>
+        /// <response code="202">Als de aanvraag om het gebouw te realiseren succesvol is.</response>
         /// <response code="202">Als de aanvraag reeds in verwerking is.</response>
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
@@ -34,7 +34,7 @@ namespace Public.Api.Building.BackOffice
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het gerealizeerde gebouw.", "")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het gerealiseerde gebouw.", "")]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", "string", "De ETag van de response.")]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
         [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
