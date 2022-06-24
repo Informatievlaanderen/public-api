7b20bc4185fea3ad2bd19816bb3484265f230d9f
Arne Dumarey <dumarey.arne@gmail.com>
2022-06-24 14:24:26 +0200

    feat: add plan & realize buildingunit

---
+++ src/Public.Api/BuildingUnit/BackOffice/BuildingUnitBackOfficerController-Plan.cs
@@ -0,0 +1,73 @@
+namespace Public.Api.BuildingUnit.BackOffice
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using BuildingRegistry.Api.BackOffice.Abstractions.BuildingUnit.Requests;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using RestSharp;
+    using Swashbuckle.AspNetCore.Annotations;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class BuildingUnitBackOfficeController
+    {
+        /// <summary>
+        /// Plan een gebouweenheid in.
+        /// </summary>
+        /// <param name="planBuildingUnitRequest"></param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="problemDetailsHelper"></param>
+        /// <param name="planBuildingUnitToggle"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="202">Als de gebouweenheid succesvol gepland is.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van de geplande gebouweenheid.", "")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", "string", "De ETag van de response.")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
+        [SwaggerRequestExample(typeof(PlanBuildingUnitRequest), typeof(PlanBuildingUnitRequestExamples))]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+        [SwaggerOperation(Description = "Voer een nieuw gebouweenheid in met status 'gepland'.")]
+        [HttpPost("gebouweenheden/acties/plannen", Name = nameof(PlanBuildingUnit))]
+        public async Task<IActionResult> PlanBuildingUnit(
+            [FromBody] PlanBuildingUnitRequest planBuildingUnitRequest,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            [FromServices] PlanBuildingUnitToggle planBuildingUnitToggle,
+            CancellationToken cancellationToken = default)
+        {
+            if (!planBuildingUnitToggle.FeatureEnabled)
+                return NotFound();
+
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
+
+            IRestRequest BackendRequest() => CreateBackendRequestWithJsonBody(
+                "gebouweenheden/acties/plannen",
+                planBuildingUnitRequest,
+                Method.POST);
+
+            var value = await GetFromBackendWithBadRequestAsync(
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    CreateDefaultHandleBadRequest(),
+                    problemDetailsHelper,
+                    cancellationToken: cancellationToken);
+
+            return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
+        }
+    }
+}
