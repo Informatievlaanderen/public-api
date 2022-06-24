5bb52d05f6e620dbc62d19c7a3503be4fa05b5c0
Arne Dumarey <dumarey.arne@gmail.com>
2022-06-24 14:24:26 +0200

    feat: add plan & realize buildingunit

---
+++ src/Public.Api/BuildingUnit/BackOffice/BuildingUnitBackOfficeController.cs
@@ -0,0 +1,42 @@
+namespace Public.Api.BuildingUnit.BackOffice
+{
+    using Autofac.Features.AttributeFilters;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Common.Infrastructure;
+    using Common.Infrastructure.Controllers;
+    using Common.Infrastructure.Controllers.Attributes;
+    using FeatureToggle;
+    using Infrastructure.Configuration;
+    using Infrastructure.Swagger;
+    using Infrastructure.Version;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using RestSharp;
+
+    [ApiVisible]
+    [ApiVersion(Version.V2)]
+    [AdvertiseApiVersions(Version.V2)]
+    [ApiRoute("")]
+    [ApiExplorerSettings(GroupName = "Gebouweenheden")]
+    [ApiOrder(Order = ApiOrder.Building)]
+    [ApiConsumes(EndpointType.BackOffice)]
+    [ApiProduces(EndpointType.BackOffice)]
+    public partial class BuildingUnitBackOfficeController : RegistryApiController<BuildingUnitBackOfficeController>
+    {
+        protected override string NotFoundExceptionMessage => "Onbestaande gebouweenheid.";
+        protected override string GoneExceptionMessage => "Verwijderde gebouweenheid.";
+
+        public BuildingUnitBackOfficeController(
+            [KeyFilter(RegistryKeys.BuildingBackOffice)]
+            IRestClient restClient,
+            [KeyFilter(RegistryKeys.BuildingBackOffice)]
+            IFeatureToggle cacheToggle,
+            ConnectionMultiplexerProvider redis,
+            ILogger<BuildingUnitBackOfficeController> logger)
+            : base(restClient, cacheToggle, redis, logger)
+        { }
+
+        private static ContentFormat DetermineFormat(ActionContext context)
+            => ContentFormat.For(EndpointType.BackOffice, context);
+    }
+}
