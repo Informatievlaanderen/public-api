36f84abe122f2cd0e15e11d7d21c62a6b64b72f3
emalfroy <egon.malfroy@vlaanderen.be>
2022-07-27 11:34:36 +0200

    feat: add not realize building

--- src/Public.Api/Building/BackOffice/BuildingBackOfficerController-NotRealize.cs
+++ src/Public.Api/Building/BackOffice/BuildingBackOfficerController-NotRealize.cs
@@ -1,9 +1,9 @@
-namespace Public.Api.BuildingUnit.BackOffice
+namespace Public.Api.Building.BackOffice
 {
     using System.Threading;
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
-    using BuildingRegistry.Api.Legacy.Abstractions.BuildingUnit.Responses;
+    using BuildingRegistry.Api.Legacy.Abstractions.Building.Responses;
     using Common.Infrastructure;
     using Infrastructure;
     using Microsoft.AspNetCore.Http;
@@ -14,22 +14,22 @@ namespace Public.Api.BuildingUnit.BackOffice
     using Swashbuckle.AspNetCore.Filters;
     using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
 
-    public partial class BuildingUnitBackOfficeController
+    public partial class BuildingBackOfficeController
     {
         /// <summary>
-        /// Realiseer een gebouweenheid niet.
+        /// Realiseer een gebouw niet.
         /// </summary>
-        /// <param name="objectId">Identificator van de gebouweenheid.</param>
+        /// <param name="objectId">Identificator van het gebouw.</param>
         /// <param name="actionContextAccessor"></param>
         /// <param name="problemDetailsHelper"></param>
-        /// <param name="notRealizeBuildingUnitToggle"></param>
-        /// <param name="ifMatch">If-Match header met ETag van de laatst gekende versie van de gebouweenheid (optioneel).</param>
+        /// <param name="realizeBuildingToggle"></param>
+        /// <param name="ifMatch">If-Match header met ETag van de laatst gekende versie van het gebouw (optioneel).</param>
         /// <param name="cancellationToken"></param>
-        /// <response code="202">Als de gebouweenheid succesvol niet gerealiseerd is.</response>
+        /// <response code="202">Als het gebouw succesvol niet gerealiseerd is.</response>
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
-        /// <response code="404">Als de gebouweenheid niet gevonden kan worden.</response>
+        /// <response code="404">Als het gebouw niet gevonden kan worden.</response>
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
-        /// <response code="410">Als de gebouweenheid verwijderd is.</response>
+        /// <response code="410">Als het gebouw verwijderd is.</response>
         /// <response code="412">Als de If-Match header niet overeenkomt met de laatste ETag.</response>
         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
@@ -41,33 +41,33 @@ namespace Public.Api.BuildingUnit.BackOffice
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status412PreconditionFailed)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van de niet gerealiseerde gebouweenheid.", "")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het niet gerealiseerde gebouw.", "")]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", "string", "De ETag van de response.")]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
         [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(BuildingUnitNotFoundResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status410Gone, typeof(BuildingUnitGoneResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(BuildingNotFoundResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status410Gone, typeof(BuildingGoneResponseExamples))]
         [SwaggerResponseExample(StatusCodes.Status412PreconditionFailed, typeof(PreconditionFailedResponseExamples))]
         [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
-        [SwaggerOperation(Description = "Wijzig de status van een gebouweenheid met status “gepland” naar “nietGerealiseerd”.")]
-        [HttpPost("gebouweenheden/{objectId}/acties/nietrealiseren", Name = nameof(NotRealizeBuildingUnit))]
-        public async Task<IActionResult> NotRealizeBuildingUnit(
+        [SwaggerOperation(Description = "Wijzig de gebouwstatus van “gepland” of “inAanbouw“ naar “NietGerealiseerd”. Onderliggende gebouweenheden worden mee “nietGerealiseerd”.")]
+        [HttpPost("gebouwen/{objectId}/acties/nietrealiseren", Name = nameof(NotRealizeBuilding))]
+        public async Task<IActionResult> NotRealizeBuilding(
             [FromRoute] int objectId,
             [FromServices] IActionContextAccessor actionContextAccessor,
             [FromServices] ProblemDetailsHelper problemDetailsHelper,
-            [FromServices] RealizeBuildingUnitToggle notRealizeBuildingUnitToggle,
+            [FromServices] NotRealizeBuildingToggle notRealizeBuildingToggle,
             [FromHeader(Name = HeaderNames.IfMatch)] string? ifMatch,
             CancellationToken cancellationToken = default)
         {
-            if (!notRealizeBuildingUnitToggle.FeatureEnabled)
+            if (!notRealizeBuildingToggle.FeatureEnabled)
                 return NotFound();
 
             var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
 
             IRestRequest BackendRequest()
             {
-                var request = new RestRequest("gebouweenheden/{persistentLocalId}/acties/nietrealiseren", Method.POST);
+                var request = new RestRequest("gebouwen/{persistentLocalId}/acties/nietrealiseren", Method.POST);
                 request.AddParameter("persistentLocalId", objectId, ParameterType.UrlSegment);
 
                 if (ifMatch is not null)
