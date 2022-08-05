c93c1644132e3af5d3cec3c77c8c1f134b788fe7
emalfroy <egon.malfroy@vlaanderen.be>
2022-08-05 16:37:49 +0200

    feat: retire streetname

---
+++ src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-Retire.cs
@@ -0,0 +1,95 @@
+namespace Public.Api.StreetName.BackOffice
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using RestSharp;
+    using StreetNameRegistry.Api.Legacy.StreetName.Responses;
+    using Swashbuckle.AspNetCore.Annotations;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class StreetNameBackOfficeController
+    {
+        /// <summary>
+        /// Hef een straatnaam op.
+        /// </summary>
+        /// <param name="objectId">Identificator van de straatnaam.</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="problemDetailsHelper"></param>
+        /// <param name="ifMatch">If-Match header met ETag van de laatst gekende versie van de straatnaam (optioneel).</param>
+        /// <param name="approveStreetNameToggle"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="202">Als de opheffing reeds in verwerking is.</response>
+        /// <response code="204">Als de straatnaam succesvol gehistoreerd is.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="404">Als de straatnaam niet gevonden kan worden.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="409">Als de straatnaam status niet 'voorgesteld' is.</response>
+        /// <response code="410">Als de straatnaam verwijderd is.</response>
+        /// <response code="412">Als de If-Match header niet overeenkomt met de laatste ETag.</response>
+        /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status410Gone)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status412PreconditionFailed)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(StreetNameNotFoundResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status409Conflict, typeof(ConflictResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status410Gone, typeof(StreetNameGoneResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status412PreconditionFailed, typeof(PreconditionFailedResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+        [SwaggerOperation(Description = "Wijzig de straatnaamstatus van `inGebruik` naar `gehistoreerd`.")]
+        [HttpPost("straatnamen/{objectId}/acties/opheffen", Name = nameof(RetireStreetName))]
+        public async Task<IActionResult> RetireStreetName(
+            [FromRoute] int objectId,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            [FromServices] RetireStreetNameToggle retireStreetNameToggle,
+            [FromHeader(Name = HeaderNames.IfMatch)] string? ifMatch,
+            CancellationToken cancellationToken = default)
+        {
+            if (!retireStreetNameToggle.FeatureEnabled)
+            {
+                return NotFound();
+            }
+
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
+
+            IRestRequest BackendRequest()
+            {
+                var request = new RestRequest("straatnamen/{persistentLocalId}/acties/opheffen", Method.POST);
+                request.AddParameter("persistentLocalId", objectId, ParameterType.UrlSegment);
+
+                if (ifMatch is not null)
+                {
+                    request.AddHeader(HeaderNames.IfMatch, ifMatch);
+                }
+
+                return request;
+            };
+
+            var value = await GetFromBackendWithBadRequestAsync(
+                contentFormat.ContentType,
+                BackendRequest,
+                CreateDefaultHandleBadRequest(),
+                problemDetailsHelper,
+                cancellationToken: cancellationToken);
+
+            return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
+        }
+    }
+}
