f605dc245270505a5c4f9fd3cae62fda1e7b1abe
Arne Dumarey <dumarey.arne@gmail.com>
2022-08-25 09:38:47 +0200

    fix: correct action names to match toggles #GAWR-3570

--- src/Public.Api/Address/BackOffice/AddressBackOfficerController-CorrectPosition.cs
+++ src/Public.Api/Address/BackOffice/AddressBackOfficerController-CorrectPosition.cs
@@ -57,8 +57,8 @@ namespace Public.Api.Address.BackOffice
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
         [SwaggerRequestExample(typeof(AddressCorrectPositionRequest), typeof(AddressCorrectPositionRequestExamples))]
         [SwaggerOperation(Description = "Correctie van de positiespecificatie, positiegeometriemethode of positie van een adres.")]
-        [HttpPost(CorrectPositionRoute, Name = nameof(CorrectAddressPosition))]
-        public async Task<IActionResult> CorrectAddressPosition(
+        [HttpPost(CorrectPositionRoute, Name = nameof(CorrectPositionAddress))]
+        public async Task<IActionResult> CorrectPositionAddress(
             [FromRoute] int objectId,
             [FromBody] AddressCorrectPositionRequest addressCorrectPositionRequest,
             [FromServices] IActionContextAccessor actionContextAccessor,
