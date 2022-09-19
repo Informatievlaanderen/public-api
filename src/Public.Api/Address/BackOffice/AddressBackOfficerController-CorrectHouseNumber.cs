215e426a8ffcc1696c5f09cdb976d5682b2c4dff
jvandaal <jonas.vandaal@outlook.be>
2022-09-19 09:37:19 +0200

    feat: correct address house number

---
+++ src/Public.Api/Address/BackOffice/AddressBackOfficerController-CorrectHouseNumber.cs
@@ -0,0 +1,110 @@
+
+namespace Public.Api.Address.BackOffice
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AddressRegistry.Api.BackOffice.Abstractions.Requests;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Common.Infrastructure;
+    using Infrastructure;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using RestSharp;
+    using AddressRegistry.Api.Legacy.Address.Responses;
+    using Infrastructure.Swagger;
+    using Newtonsoft.Json;
+    using Swashbuckle.AspNetCore.Annotations;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class AddressBackOfficeController
+    {
+        public const string CorrectHouseNumberRoute = "adressen/{objectId}/acties/corrigeren/huisnummer";
+
+        /// <summary>
+        /// Corrigeer het huisnummer van een adres.
+        /// </summary>
+        /// <param name="objectId">Identificator van het adres.</param>
+        /// <param name="addressCorrectHouseNumberRequest"></param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="problemDetailsHelper"></param>
+        /// <param name="changeHouseNumberAddressToggle"></param>
+        /// <param name="ifMatch">If-Match header met ETag van de laatst gekende versie van het adres (optioneel).</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="202">Als de postcode van een adres succesvol gecorrigeerd is.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="404">Als het adres niet gevonden kan worden.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="410">Als het adres verwijderd is.</response>
+        /// <response code="412">Als de If-Match header niet overeenkomt met de laatste ETag.</response>
+        /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        /// <returns></returns>
+        [ApiOrder(ApiOrder.Address.Edit + 30)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status410Gone)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status412PreconditionFailed)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het gecorrigeerde adres.", "")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", "string", "De ETag van de response.")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(AddressNotFoundResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status410Gone, typeof(AddressGoneResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status412PreconditionFailed, typeof(PreconditionFailedResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+        [SwaggerRequestExample(typeof(AddressChangePositionRequest), typeof(AddressChangePositionRequestExamples))]
+        [SwaggerOperation(Description = "Correctie van het huisnummer van een adres.")]
+        [HttpPost(CorrectHouseNumberRoute, Name = nameof(CorrectHouseNumberAddress))]
+        public async Task<IActionResult> CorrectHouseNumberAddress(
+            [FromRoute] int objectId,
+            [FromBody] AddressCorrectHouseNumberRequest addressCorrectHouseNumberRequest,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            [FromServices] CorrectHouseNumberAddress changeHouseNumberAddressToggle,
+            [FromHeader(Name = HeaderNames.IfMatch)] string? ifMatch,
+            CancellationToken cancellationToken = default)
+        {
+            if (!changeHouseNumberAddressToggle.FeatureEnabled)
+            {
+                return NotFound();
+            }
+
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
+
+            IRestRequest BackendRequest()
+            {
+                var request = new RestRequest(CorrectHouseNumberRoute, Method.POST)
+                    .AddParameter(
+                        "application/json; charset=utf-8",
+                        JsonConvert.SerializeObject(addressCorrectHouseNumberRequest),
+                        ParameterType.RequestBody)
+                    .AddParameter(
+                        "objectId",
+                        objectId,
+                        ParameterType.UrlSegment);
+
+                if (ifMatch is not null)
+                {
+                    request.AddHeader(HeaderNames.IfMatch, ifMatch);
+                }
+
+                return request;
+            }
+
+            var value = await GetFromBackendWithBadRequestAsync(
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    CreateDefaultHandleBadRequest(),
+                    problemDetailsHelper,
+                    cancellationToken: cancellationToken);
+
+            return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
+        }
+    }
+}
