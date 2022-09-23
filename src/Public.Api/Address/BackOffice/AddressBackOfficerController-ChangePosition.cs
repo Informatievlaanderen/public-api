c3c7b216934b4b0bb51f96e4f8439512f1d56409
pgallik <patrickgallik@gmail.com>
2022-09-23 10:58:19 +0200

    fix: endpoint ordering address & streetname

--- src/Public.Api/Address/BackOffice/AddressBackOfficerController-ChangePosition.cs
+++ src/Public.Api/Address/BackOffice/AddressBackOfficerController-ChangePosition.cs
@@ -41,7 +41,7 @@ namespace Public.Api.Address.BackOffice
         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         /// <returns></returns>
-        [ApiOrder(ApiOrder.Address.Edit + 8)]
+        [ApiOrder(ApiOrder.Address.Edit + 10)]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
--- src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-CorrectNames.cs
+++ src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-CorrectNames.cs
@@ -40,7 +40,7 @@ namespace Public.Api.StreetName.BackOffice
         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         /// <returns></returns>
-        [ApiOrder(ApiOrder.StreetName.V2 + 5)]
+        [ApiOrder(ApiOrder.StreetName.Edit + 5)]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
