98ce13ed59f494e7c106a44080776787c07ac5dd
jvandaal <jonas.vandaal@outlook.be>
2022-09-29 15:06:06 +0200

    Revert "feat: make hidden api methods configurable"
    This reverts commit 232835ce883a8843aca05a25949c72b0d7c842e0.

--- src/Public.Api/Infrastructure/Swagger/ApiDocumentationHiddenConvention.cs
+++ src/Public.Api/Infrastructure/Swagger/ApiDocumentationHiddenConvention.cs
@@ -1,21 +1,22 @@
 namespace Public.Api.Infrastructure.Swagger
 {
     using System.Collections.Generic;
-    using System.Linq;
     using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
     public class ApiDocumentationHiddenConvention : IActionModelConvention
     {
-        private readonly IEnumerable<string> _hiddenMethods;
-
-        public ApiDocumentationHiddenConvention(IEnumerable<string> hiddenMethods)
-        {
-            _hiddenMethods = hiddenMethods;
-        }
-
         public void Apply(ActionModel action)
         {
-            if (_hiddenMethods.Contains(action.ActionMethod.Name))
+            var hiddenMethods = new List<string>
+            {
+                "CompleteTicket",
+                "CreateTicket",
+                "DeleteTicket",
+                "GetTickets",
+                "PendingTicket"
+            };
+
+            if (hiddenMethods.Contains(action.ActionMethod.Name))
             {
                 action.ApiExplorer.IsVisible = false;
             }
