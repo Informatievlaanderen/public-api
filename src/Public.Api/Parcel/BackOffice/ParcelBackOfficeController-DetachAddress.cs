1d08d825205cda34752db6ba4afe0f7f65c42809
pgallik <patrickgallik@gmail.com>
2022-12-02 13:45:56 +0100

    feat: add attach/dettach address

---
+++ src/Public.Api/Parcel/BackOffice/ParcelBackOfficeController-DetachAddress.cs
@@ -0,0 +1,88 @@
+namespace Public.Api.Parcel.BackOffice
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Common.Infrastructure;
+    using Common.Infrastructure.Extensions;
+    using Infrastructure;
+    using Infrastructure.Swagger;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using ParcelRegistry.Api.BackOffice.Abstractions.Requests;
+    using ParcelRegistry.Api.Legacy.Parcel.Responses;
+    using RestSharp;
+    using Swashbuckle.AspNetCore.Annotations;
+    using Swashbuckle.AspNetCore.Filters;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+
+    public partial class ParcelBackOfficeController
+    {
+        public const string DetachAddressParcelRoute = "percelen/{objectId}/acties/adresontkoppelen";
+
+        /// <summary>
+        /// Ontkoppel een perceel van een adres.
+        /// </summary>
+        /// <param name="objectId">Identificator van het perceel.</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="problemDetailsHelper"></param>
+        /// <param name="ifMatch">If-Match header met ETag van de laatst gekende versie van het perceel (optioneel).</param>
+        /// <param name="toggle"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="202">Als het ticket succesvol is aangemaakt.</response>
+        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="404">Als het adres niet gevonden kan worden.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="412">Als de If-Match header niet overeenkomt met de laatste ETag.</response>
+        /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        /// <returns></returns>
+        [ApiOrder(ApiOrder.Parcel.Edit + 2)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status412PreconditionFailed)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het aangemaakte ticket.")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(ParcelNotFoundResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status412PreconditionFailed, typeof(PreconditionFailedResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+        [SwaggerRequestExample(typeof(DetachAddressRequest), typeof(DetachAddressRequest))]
+        [SwaggerOperation(Description = "Verwijderen van een adreskoppeling op een perceelsobject.")]
+        [HttpPost(DetachAddressParcelRoute, Name = nameof(DetachAddress))]
+        public async Task<IActionResult> DetachAddress(
+            [FromBody] DetachAddressRequest request,
+            [FromRoute] int objectId,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            [FromServices] DetachAddressParcelToggle toggle,
+            [FromHeader(Name = HeaderNames.IfMatch)] string? ifMatch,
+            CancellationToken cancellationToken = default)
+        {
+            if (!toggle.FeatureEnabled)
+            {
+                return NotFound();
+            }
+
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
+
+            RestRequest BackendRequest() => CreateBackendRequestWithJsonBody(DetachAddressParcelRoute, request, Method.Post)
+                .AddParameter("objectId", objectId, ParameterType.UrlSegment)
+                .AddHeaderIfMatch(HeaderNames.IfMatch, ifMatch);
+
+            var value = await GetFromBackendWithBadRequestAsync(
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    CreateDefaultHandleBadRequest(),
+                    problemDetailsHelper,
+                    cancellationToken: cancellationToken);
+
+            return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
+        }
+    }
+}
