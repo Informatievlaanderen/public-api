c89fbb98b0d7f264cd14037aa888b5fce3c0f945
jvandaal <jonas.vandaal@outlook.be>
2023-03-06 11:21:14 +0100

    feat: remove streetname

--- src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-CorrectNames.cs
+++ src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-CorrectNames.cs
@@ -41,7 +41,7 @@ namespace Public.Api.StreetName.BackOffice
         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         /// <returns></returns>
-        [ApiOrder(ApiOrder.StreetName.Edit + 5)]
+        [ApiOrder(ApiOrder.StreetName.Edit + 6)]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
