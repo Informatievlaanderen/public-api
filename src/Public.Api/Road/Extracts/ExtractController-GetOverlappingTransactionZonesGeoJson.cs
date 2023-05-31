8e90c5206aae06bb3386f0b05c7c87b8246e41ab
Rik De Peuter <rikdepeuter@gmail.com>
2023-05-31 10:58:10 +0200

    fix: geojson endpoints allow anonymous

--- src/Public.Api/Road/Extracts/ExtractController-GetOverlappingTransactionZonesGeoJson.cs
+++ src/Public.Api/Road/Extracts/ExtractController-GetOverlappingTransactionZonesGeoJson.cs
@@ -5,11 +5,13 @@ namespace Public.Api.Road.Extracts
     using Be.Vlaanderen.Basisregisters.Api;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Infrastructure;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using RestSharp;
 
     public partial class ExtractController
     {
+        [AllowAnonymous]
         [HttpGet("wegen/extract/overlappingtransactionzones.geojson")]
         public async Task<ActionResult> GetOverlappingTransactionZonesGeoJson(
             [FromServices] ProblemDetailsHelper problemDetailsHelper,
--- src/Public.Api/Road/Extracts/ExtractController-GetTransactionZonesGeoJson.cs
+++ src/Public.Api/Road/Extracts/ExtractController-GetTransactionZonesGeoJson.cs
@@ -5,11 +5,13 @@ namespace Public.Api.Road.Extracts
     using Be.Vlaanderen.Basisregisters.Api;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Infrastructure;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using RestSharp;
 
     public partial class ExtractController
     {
+        [AllowAnonymous]
         [HttpGet("wegen/extract/transactionzones.geojson")]
         public async Task<ActionResult> GetTransactionZonesGeoJson(
             [FromServices] ProblemDetailsHelper problemDetailsHelper,
