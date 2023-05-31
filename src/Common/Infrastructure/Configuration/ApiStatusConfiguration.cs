6c5bf5e0d6171f89ff7918715a5a05d0d2a07c8e
emalfroy <egon.malfroy@gmail.com>
2023-05-31 16:16:17 +0200

    feat: add producer status for statuspage

--- src/Common/Infrastructure/Configuration/ApiStatusConfiguration.cs
+++ src/Common/Infrastructure/Configuration/ApiStatusConfiguration.cs
@@ -4,5 +4,7 @@ namespace Common.Infrastructure.Configuration
     {
         public string ImportUrl { get; set; }
         public string ProjectionsUrl { get; set; }
+        public string ProducerUrl { get; set; }
+        public string ProducerSnapshotOsloUrl { get; set; }
     }
 }
---
+++ src/Public.Api/Status/Clients/ProducerSnapshotOsloStatusClient.cs
@@ -0,0 +1,34 @@
+namespace Public.Api.Status.Clients
+{
+    using System.Linq;
+    using BackendResponse;
+    using Common.Infrastructure;
+    using Responses;
+    using RestSharp;
+
+    public class ProducerSnapshotOsloStatusClient : BaseStatusClient<RegistryProjectionStatusResponse, ProjectionsStatusList>
+    {
+        public ProducerSnapshotOsloStatusClient(string registry, TraceRestClient restClient)
+            : base(registry, restClient) { }
+
+        protected override RestRequest CreateStatusRequest()
+            => new RestRequest("projections");
+
+        protected override RegistryProjectionStatusResponse Map(ProjectionsStatusList response)
+            => new RegistryProjectionStatusResponse
+            {
+                StreamPosition = response.StreamPosition,
+                Projections = response
+                    .Projections
+                    .Select(status =>
+                        new RegistryProjectionStatus
+                        {
+                            Key = status.Id,
+                            Name = string.IsNullOrWhiteSpace(status.Name) ? status.Id : status.Name,
+                            Description = status.Description,
+                            State = status.State,
+                            CurrentPosition = status.CurrentPosition
+                        })
+            };
+    }
+}
