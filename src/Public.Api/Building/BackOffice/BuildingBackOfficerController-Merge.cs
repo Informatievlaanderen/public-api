9752d9653207acdcb719df7f45eb682bd06360b9
Arne Dumarey <dumarey.arne@gmail.com>
2023-06-28 14:44:14 +0200

    feat: add merge building + expand order range

--- src/Public.Api/Building/BackOffice/BuildingBackOfficerController-Merge.cs
+++ src/Public.Api/Building/BackOffice/BuildingBackOfficerController-Merge.cs
@@ -4,7 +4,6 @@ namespace Public.Api.Building.BackOffice
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using BuildingRegistry.Api.BackOffice.Abstractions.Building.Requests;
-    using BuildingRegistry.Api.Oslo.Building.Detail;
     using Common.Infrastructure;
     using Common.Infrastructure.Extensions;
     using Infrastructure;
@@ -19,75 +18,64 @@ namespace Public.Api.Building.BackOffice
 
     public partial class BuildingBackOfficeController
     {
-        public const string CorrectBuildingGeometryOutlineRoute = "gebouwen/{objectId}/acties/wijzigen/schetsgeometriepolygoon";
+        public const string MergeBuildingRoute = "gebouwen/acties/samenvoegen";
 
         /// <summary>
-        /// Wijzig de geometrie van een geschetst gebouw.
+        /// Samenvoegen tot één gebouw.
         /// </summary>
-        /// <param name="objectId">Identificator van het gebouw.</param>
-        /// <param name="changeBuildingOutlineRequest"></param>
+        /// <param name="mergeBuildingRequest"></param>
         /// <param name="actionContextAccessor"></param>
         /// <param name="problemDetailsHelper"></param>
-        /// <param name="changeBuildingGeometryOutlineToggle"></param>
-        /// <param name="ifMatch">If-Match header met ETag van de laatst gekende versie van het gebouw (optioneel).</param>
+        /// <param name="mergeBuildingToggle"></param>
         /// <param name="cancellationToken"></param>
         /// <response code="202">Als het ticket succesvol is aangemaakt.</response>
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
         /// <response code="401">Als u niet geauthenticeerd bent om deze actie uit te voeren.</response>
         /// <response code="403">Als u niet beschikt over de correcte rechten om deze actie uit te voeren.</response>
-        /// <response code="404">Als het gebouw niet gevonden kan worden.</response>
         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
-        /// <response code="412">Als de If-Match header niet overeenkomt met de laatste ETag.</response>
         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         /// <returns></returns>
-        [ApiOrder(ApiOrder.Building.Edit + 90)]
+        [ApiOrder(ApiOrder.Building.Edit + 10)]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
         [ProducesResponseType(typeof(Be.Vlaanderen.Basisregisters.BasicApiProblem.ValidationProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status412PreconditionFailed)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerRequestExample(typeof(MergeBuildingRequest), typeof(MergeBuildingRequestExamples))]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het aangemaakte ticket.")]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
         [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamplesV2))]
         [SwaggerResponseExample(StatusCodes.Status401Unauthorized, typeof(UnauthorizedOAuthResponseExamplesV2))]
         [SwaggerResponseExample(StatusCodes.Status403Forbidden, typeof(ForbiddenOAuthResponseExamplesV2))]
-        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(BuildingNotFoundResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status412PreconditionFailed, typeof(PreconditionFailedResponseExamplesV2))]
         [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamplesV2))]
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamplesV2))]
-        [SwaggerOperation(Description = "Wijzig de geometrie van een geschetst gebouw. De gekoppelde gebouweenheden moeten in deze nieuwe geometrie liggen.")]
-        [HttpPost(CorrectBuildingGeometryOutlineRoute, Name = nameof(ChangeBuildingGeometryOutline))]
-        public async Task<IActionResult> ChangeBuildingGeometryOutline(
-            [FromRoute] int objectId,
-            [FromBody] ChangeBuildingOutlineRequest changeBuildingOutlineRequest,
-            [FromServices] IActionContextAccessor actionContextAccessor,
-            [FromServices] ProblemDetailsHelper problemDetailsHelper,
-            [FromServices] ChangeBuildingGeometryOutlineToggle changeBuildingGeometryOutlineToggle,
-            [FromHeader(Name = HeaderNames.IfMatch)] string? ifMatch,
-            CancellationToken cancellationToken = default)
+        [SwaggerOperation(Description = "Voeg verschillende gebouwen samen tot één gebouw.")]
+        [HttpPost(MergeBuildingRoute, Name = nameof(MergeBuilding))]
+        public async Task<IActionResult> MergeBuilding(
+                    [FromBody] MergeBuildingRequest mergeBuildingRequest,
+                    [FromServices] IActionContextAccessor actionContextAccessor,
+                    [FromServices] ProblemDetailsHelper problemDetailsHelper,
+                    [FromServices] MergeBuildingToggle mergeBuildingToggle,
+                    CancellationToken cancellationToken = default)
         {
-            if (!changeBuildingGeometryOutlineToggle.FeatureEnabled)
+            if (!mergeBuildingToggle.FeatureEnabled)
             {
                 return NotFound();
             }
 
             var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
 
-            RestRequest BackendRequest () => CreateBackendRequestWithJsonBody(CorrectBuildingGeometryOutlineRoute, changeBuildingOutlineRequest, Method.Post)
-                .AddParameter("objectId", objectId, ParameterType.UrlSegment)
-                .AddHeaderIfMatch(ifMatch)
-                .AddHeaderAuthorization(actionContextAccessor);
+            RestRequest BackendRequest() =>
+                CreateBackendRequestWithJsonBody(MergeBuildingRoute, mergeBuildingRequest, Method.Post).AddHeaderAuthorization(actionContextAccessor);
 
             var value = await GetFromBackendWithBadRequestAsync(
-                contentFormat.ContentType,
-                BackendRequest,
-                CreateDefaultHandleBadRequest(),
-                problemDetailsHelper,
-                cancellationToken: cancellationToken);
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    CreateDefaultHandleBadRequest(),
+                    problemDetailsHelper,
+                    cancellationToken: cancellationToken);
 
             return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
         }
