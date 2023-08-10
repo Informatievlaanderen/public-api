e43e3518d831f9e351c9448ee04b33b9cf7f3efc
emalfroy <egon.malfroy@gmail.com>
2023-08-10 11:22:56 +0200

    feat: add status for importer grb

---
+++ src/Public.Api/Status/Clients/ImporterGrbStatusClient.cs
@@ -0,0 +1,42 @@
+namespace Public.Api.Status.Clients
+{
+    using System.Collections.Generic;
+    using BackendResponse;
+    using Common.Infrastructure;
+    using Responses;
+    using RestSharp;
+
+    public class ImporterGrbStatusClient : BaseStatusClient<IEnumerable<RegistryImportStatus>, ImportStatus>
+    {
+        public ImporterGrbStatusClient(string registry, TraceRestClient restClient)
+            : base(registry, restClient) { }
+
+        protected override RestRequest CreateStatusRequest()
+            => new RestRequest("importergrb");
+
+        protected override IEnumerable<RegistryImportStatus> Map(ImportStatus response)
+            => new List<RegistryImportStatus>{MapToImportStatusResponse(response)};
+
+        private static RegistryImportStatus MapToImportStatusResponse(ImportStatus status)
+        {
+            RegistryImportBatch MapImportBatch(ImportStatusBatchScope batch)
+            {
+                if (batch == null)
+                    return null;
+
+                return new RegistryImportBatch
+                {
+                    From = batch.From.UtcDateTime,
+                    Until = batch.Until.UtcDateTime
+                };
+            }
+
+            return new RegistryImportStatus
+            {
+                Name = status.Name,
+                LastCompletedImport = MapImportBatch(status.LastCompletedBatch),
+                CurrentImport = MapImportBatch(status.CurrentBatch)
+            };
+        }
+    }
+}
