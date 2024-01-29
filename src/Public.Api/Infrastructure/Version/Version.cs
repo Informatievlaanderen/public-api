a70d0d96e5f8b56080462be7dc3ebb81608c6f4f
Arne Dumarey <dumarey.arne@gmail.com>
2024-01-29 11:23:48 +0100

    fix: change advertised to `2.0`

--- src/Public.Api/Infrastructure/Version/Version.cs
+++ src/Public.Api/Infrastructure/Version/Version.cs
@@ -3,7 +3,7 @@ namespace Public.Api.Infrastructure.Version
     public static class Version
     {
         public const string Current = "1.0";
-        public const string CurrentAdvertised = "1.0";
+        public const string CurrentAdvertised = "2.0";
         public const string V2 = "2.0";
     }
 }
