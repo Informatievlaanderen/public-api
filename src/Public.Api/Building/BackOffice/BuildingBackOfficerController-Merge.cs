e512ac5bfafbfb61608735e7923ee8dbd3627bd8
Arne Dumarey <dumarey.arne@gmail.com>
2024-02-08 12:33:59 +0100

    docs: disable merge building

--- src/Public.Api/Building/BackOffice/BuildingBackOfficerController-Merge.cs
+++ src/Public.Api/Building/BackOffice/BuildingBackOfficerController-Merge.cs
@@ -1,83 +1,83 @@
-namespace Public.Api.Building.BackOffice
-{
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
-    using BuildingRegistry.Api.BackOffice.Abstractions.Building.Requests;
-    using Common.Infrastructure;
-    using Common.Infrastructure.Extensions;
-    using Infrastructure;
-    using Infrastructure.Swagger;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.Infrastructure;
-    using RestSharp;
-    using Swashbuckle.AspNetCore.Annotations;
-    using Swashbuckle.AspNetCore.Filters;
-    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
-
-    public partial class BuildingBackOfficeController
-    {
-        public const string MergeBuildingRoute = "gebouwen/acties/samenvoegen";
-
-        /// <summary>
-        /// Samenvoegen tot één gebouw.
-        /// </summary>
-        /// <param name="mergeBuildingRequest"></param>
-        /// <param name="actionContextAccessor"></param>
-        /// <param name="problemDetailsHelper"></param>
-        /// <param name="mergeBuildingToggle"></param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="202">Als het ticket succesvol is aangemaakt.</response>
-        /// <response code="400">Als uw verzoek foutieve data bevat.</response>
-        /// <response code="401">Als u niet geauthenticeerd bent om deze actie uit te voeren.</response>
-        /// <response code="403">Als u niet beschikt over de correcte rechten om deze actie uit te voeren.</response>
-        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
-        /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
-        /// <response code="500">Als er een interne fout is opgetreden.</response>
-        /// <returns></returns>
-        [ApiOrder(ApiOrder.Building.Edit + 10)]
-        [ProducesResponseType(StatusCodes.Status202Accepted)]
-        [ProducesResponseType(typeof(Be.Vlaanderen.Basisregisters.BasicApiProblem.ValidationProblemDetails), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerRequestExample(typeof(MergeBuildingRequest), typeof(MergeBuildingRequestExamples))]
-        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het aangemaakte ticket.")]
-        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
-        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamplesV2))]
-        [SwaggerResponseExample(StatusCodes.Status401Unauthorized, typeof(UnauthorizedOAuthResponseExamplesV2))]
-        [SwaggerResponseExample(StatusCodes.Status403Forbidden, typeof(ForbiddenOAuthResponseExamplesV2))]
-        [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamplesV2))]
-        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamplesV2))]
-        [SwaggerOperation(Description = "Voeg verschillende gebouwen samen tot één gebouw.")]
-        [HttpPost(MergeBuildingRoute, Name = nameof(MergeBuilding))]
-        public async Task<IActionResult> MergeBuilding(
-                    [FromBody] MergeBuildingRequest mergeBuildingRequest,
-                    [FromServices] IActionContextAccessor actionContextAccessor,
-                    [FromServices] ProblemDetailsHelper problemDetailsHelper,
-                    [FromServices] MergeBuildingToggle mergeBuildingToggle,
-                    CancellationToken cancellationToken = default)
-        {
-            if (!mergeBuildingToggle.FeatureEnabled)
-            {
-                return NotFound();
-            }
-
-            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
-
-            RestRequest BackendRequest() =>
-                CreateBackendRequestWithJsonBody(MergeBuildingRoute, mergeBuildingRequest, Method.Post).AddHeaderAuthorization(actionContextAccessor);
-
-            var value = await GetFromBackendWithBadRequestAsync(
-                    contentFormat.ContentType,
-                    BackendRequest,
-                    CreateDefaultHandleBadRequest(),
-                    problemDetailsHelper,
-                    cancellationToken: cancellationToken);
-
-            return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
-        }
-    }
-}
+// namespace Public.Api.Building.BackOffice
+// {
+//     using System.Threading;
+//     using System.Threading.Tasks;
+//     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+//     using BuildingRegistry.Api.BackOffice.Abstractions.Building.Requests;
+//     using Common.Infrastructure;
+//     using Common.Infrastructure.Extensions;
+//     using Infrastructure;
+//     using Infrastructure.Swagger;
+//     using Microsoft.AspNetCore.Http;
+//     using Microsoft.AspNetCore.Mvc;
+//     using Microsoft.AspNetCore.Mvc.Infrastructure;
+//     using RestSharp;
+//     using Swashbuckle.AspNetCore.Annotations;
+//     using Swashbuckle.AspNetCore.Filters;
+//     using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+//
+//     public partial class BuildingBackOfficeController
+//     {
+//         public const string MergeBuildingRoute = "gebouwen/acties/samenvoegen";
+//
+//         /// <summary>
+//         /// Samenvoegen tot één gebouw.
+//         /// </summary>
+//         /// <param name="mergeBuildingRequest"></param>
+//         /// <param name="actionContextAccessor"></param>
+//         /// <param name="problemDetailsHelper"></param>
+//         /// <param name="mergeBuildingToggle"></param>
+//         /// <param name="cancellationToken"></param>
+//         /// <response code="202">Als het ticket succesvol is aangemaakt.</response>
+//         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+//         /// <response code="401">Als u niet geauthenticeerd bent om deze actie uit te voeren.</response>
+//         /// <response code="403">Als u niet beschikt over de correcte rechten om deze actie uit te voeren.</response>
+//         /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+//         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+//         /// <response code="500">Als er een interne fout is opgetreden.</response>
+//         /// <returns></returns>
+//         [ApiOrder(ApiOrder.Building.Edit + 10)]
+//         [ProducesResponseType(StatusCodes.Status202Accepted)]
+//         [ProducesResponseType(typeof(Be.Vlaanderen.Basisregisters.BasicApiProblem.ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+//         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+//         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+//         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
+//         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+//         [SwaggerRequestExample(typeof(MergeBuildingRequest), typeof(MergeBuildingRequestExamples))]
+//         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", "string", "De URL van het aangemaakte ticket.")]
+//         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", "string", "Correlatie identificator van de response.")]
+//         [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamplesV2))]
+//         [SwaggerResponseExample(StatusCodes.Status401Unauthorized, typeof(UnauthorizedOAuthResponseExamplesV2))]
+//         [SwaggerResponseExample(StatusCodes.Status403Forbidden, typeof(ForbiddenOAuthResponseExamplesV2))]
+//         [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamplesV2))]
+//         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamplesV2))]
+//         [SwaggerOperation(Description = "Voeg verschillende gebouwen samen tot één gebouw.")]
+//         [HttpPost(MergeBuildingRoute, Name = nameof(MergeBuilding))]
+//         public async Task<IActionResult> MergeBuilding(
+//                     [FromBody] MergeBuildingRequest mergeBuildingRequest,
+//                     [FromServices] IActionContextAccessor actionContextAccessor,
+//                     [FromServices] ProblemDetailsHelper problemDetailsHelper,
+//                     [FromServices] MergeBuildingToggle mergeBuildingToggle,
+//                     CancellationToken cancellationToken = default)
+//         {
+//             if (!mergeBuildingToggle.FeatureEnabled)
+//             {
+//                 return NotFound();
+//             }
+//
+//             var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
+//
+//             RestRequest BackendRequest() =>
+//                 CreateBackendRequestWithJsonBody(MergeBuildingRoute, mergeBuildingRequest, Method.Post).AddHeaderAuthorization(actionContextAccessor);
+//
+//             var value = await GetFromBackendWithBadRequestAsync(
+//                     contentFormat.ContentType,
+//                     BackendRequest,
+//                     CreateDefaultHandleBadRequest(),
+//                     problemDetailsHelper,
+//                     cancellationToken: cancellationToken);
+//
+//             return new BackendResponseResult(value, BackendResponseResultOptions.ForBackOffice());
+//         }
+//     }
+// }
