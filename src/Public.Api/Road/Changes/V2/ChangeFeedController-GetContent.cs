519a8a854ac1d68b2929ba220cd3720af6bd0ad6
Rik De Peuter <rikdepeuter@gmail.com>
2024-03-28 16:40:59 +0100

    update basisregisters.api package

--- src/Public.Api/Road/Changes/V2/ChangeFeedController-GetContent.cs
+++ src/Public.Api/Road/Changes/V2/ChangeFeedController-GetContent.cs
@@ -1,16 +1,18 @@
 namespace Public.Api.Road.Changes.V2
 {
-    using System.Threading;
-    using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Microsoft.AspNetCore.Mvc;
     using Public.Api.Infrastructure;
     using RestSharp;
+    using Swashbuckle.AspNetCore.Annotations;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     public partial class ChangeFeedControllerV2
     {
         [HttpGet("wegen/activiteit/gebeurtenis/{id}/inhoud", Name = nameof(GetContentV2))]
+        [SwaggerOperation(OperationId = nameof(GetContentV2))]
         public async Task<IActionResult> GetContentV2(
             [FromRoute] long? id,
             [FromServices] ProblemDetailsHelper problemDetailsHelper,
