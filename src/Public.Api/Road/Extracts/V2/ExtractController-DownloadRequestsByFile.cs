ff071286bac84c04bf5fc9387ecaa03b30e82f1c
Rik De Peuter <rikdepeuter@gmail.com>
2025-01-24 10:04:33 +0100

    refactor: add debug logging

--- src/Public.Api/Road/Extracts/V2/ExtractController-DownloadRequestsByFile.cs
+++ src/Public.Api/Road/Extracts/V2/ExtractController-DownloadRequestsByFile.cs
@@ -1,13 +1,16 @@
 namespace Public.Api.Road.Extracts.V2
 {
     using System.IO;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Common.Infrastructure.Controllers.Attributes;
     using Infrastructure;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
     using RestSharp;
     using RoadRegistry.BackOffice.Api.Extracts;
 
@@ -19,8 +22,41 @@ namespace Public.Api.Road.Extracts.V2
         public async Task<ActionResult> PostDownloadRequestByFileV2(
             DownloadExtractByFileRequestBody body,
             [FromServices] ProblemDetailsHelper problemDetailsHelper,
+            [FromServices] ILogger<ExtractControllerV2> logger,
             CancellationToken cancellationToken = default)
         {
+            var sb = new StringBuilder();
+            var context = HttpContext;
+            sb.AppendLine("Headers:");
+            foreach (var header in HttpContext.Request.Headers)
+            {
+                sb.AppendLine($"{header.Key}={header.Value}");
+            }
+            sb.AppendLine();
+
+            if (context.Request.HasFormContentType)
+            {
+                sb.AppendLine("Form data:");
+                foreach (var field in context.Request.Form)
+                {
+                    sb.AppendLine($"{field.Key}: {field.Value}");
+                }
+                sb.AppendLine();
+            }
+
+            context.Request.EnableBuffering();
+            using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8, leaveOpen: true))
+            {
+                var bodyString = await reader.ReadToEndAsync();
+                sb.AppendLine("Body:");
+                sb.AppendLine(bodyString);
+                context.Request.Body.Position = 0; // Reset the stream position
+                sb.AppendLine();
+            }
+
+            logger.LogWarning(sb.ToString());
+
+
             var response = await GetFromBackendWithBadRequestAsync(
                 AcceptType.Json,
                 BackendRequest,
@@ -36,16 +72,20 @@ namespace Public.Api.Road.Extracts.V2
 
                 request.AddParameter(nameof(body.Description), body.Description, ParameterType.GetOrPost);
                 request.AddParameter(nameof(body.IsInformative), body.IsInformative, ParameterType.GetOrPost);
-                foreach (var file in body.Files)
+                if (body.Files is not null)
                 {
-                    request.AddFile(nameof(body.Files), () =>
+                    foreach (var file in body.Files)
                     {
-                        var copyStream = new MemoryStream();
-                        file.OpenReadStream().CopyTo(copyStream);
-                        copyStream.Position = 0;
-                        return copyStream;
-                    }, file.FileName);
+                        request.AddFile(nameof(body.Files), () =>
+                        {
+                            var copyStream = new MemoryStream();
+                            file.OpenReadStream().CopyTo(copyStream);
+                            copyStream.Position = 0;
+                            return copyStream;
+                        }, file.FileName);
+                    }
                 }
+
                 request.AddHeader("Content-Type", "multipart/form-data");
 
                 return request;
