c625f0b40c6337ff5fa8296e2fc20e803c3ec225
Rik De Peuter <rikdepeuter@gmail.com>
2025-01-24 11:51:58 +0100

    refactor: remove debug logging

--- src/Public.Api/Road/Extracts/V2/ExtractController-DownloadRequestsByFile.cs
+++ src/Public.Api/Road/Extracts/V2/ExtractController-DownloadRequestsByFile.cs
@@ -1,14 +1,12 @@
 namespace Public.Api.Road.Extracts.V2
 {
     using System.IO;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Common.Infrastructure.Controllers.Attributes;
     using Infrastructure;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using RestSharp;
@@ -25,38 +23,6 @@ namespace Public.Api.Road.Extracts.V2
             [FromServices] ILogger<ExtractControllerV2> logger,
             CancellationToken cancellationToken = default)
         {
-            var sb = new StringBuilder();
-            var context = HttpContext;
-            sb.AppendLine("Headers:");
-            foreach (var header in HttpContext.Request.Headers)
-            {
-                sb.AppendLine($"{header.Key}={header.Value}");
-            }
-            sb.AppendLine();
-
-            if (context.Request.HasFormContentType)
-            {
-                sb.AppendLine("Form data:");
-                foreach (var field in context.Request.Form)
-                {
-                    sb.AppendLine($"{field.Key}: {field.Value}");
-                }
-                sb.AppendLine();
-            }
-
-            context.Request.EnableBuffering();
-            using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8, leaveOpen: true))
-            {
-                var bodyString = await reader.ReadToEndAsync();
-                sb.AppendLine("Body:");
-                sb.AppendLine(bodyString);
-                context.Request.Body.Position = 0; // Reset the stream position
-                sb.AppendLine();
-            }
-
-            logger.LogWarning(sb.ToString());
-
-
             var response = await GetFromBackendWithBadRequestAsync(
                 AcceptType.Json,
                 BackendRequest,
