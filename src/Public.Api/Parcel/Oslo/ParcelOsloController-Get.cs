6f98e659492cfc37c77cd89f9e21f33b343fd9e9
Arne Dumarey <dumarey.arne@gmail.com>
2025-06-17 15:09:31 +0200

    feat: add parcel capakey route GAWR-6952

--- src/Public.Api/Parcel/Oslo/ParcelOsloController-Get.cs
+++ src/Public.Api/Parcel/Oslo/ParcelOsloController-Get.cs
@@ -20,7 +20,7 @@ namespace Public.Api.Parcel.Oslo
         /// <summary>
         /// Vraag een perceel op (v2).
         /// </summary>
-        /// <param name="objectId">Objectidentificator van het perceel (CaPaKey waarbij forward slash `/` vervangen werd door koppelteken `-`).</param>
+        /// <param name="objectId">Objectidentificator van het perceel (CaPaKey waarbij forward slash `/` vervangen werd door koppelteken `-`) of de URL-gecodeerde CaPaKey.</param>
         /// <param name="actionContextAccessor"></param>
         /// <param name="ifNoneMatch">If-None-Match header met ETag van een vorig verzoek (optioneel). </param>
         /// <param name="cancellationToken"></param>
@@ -54,6 +54,69 @@ namespace Public.Api.Parcel.Oslo
             [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
             CancellationToken cancellationToken = default)
         {
+            objectId = objectId.Replace("/", "-");
+            var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
+
+            RestRequest BackendRequest() => CreateBackendDetailRequest(objectId);
+
+            var cacheKey = $"oslo/parcel:{objectId}";
+
+            var value = await (CanGetFromCache(actionContextAccessor.ActionContext)
+                ? GetFromCacheThenFromBackendAsync(
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    cacheKey,
+                    CreateDefaultHandleBadRequest(),
+                    cancellationToken)
+                : GetFromBackendAsync(
+                    contentFormat.ContentType,
+                    BackendRequest,
+                    CreateDefaultHandleBadRequest(),
+                    cancellationToken));
+
+            return new BackendResponseResult(value, BackendResponseResultOptions.ForRead());
+        }
+
+        /// <summary>
+        /// Vraag een perceel op via CaPaKey (v2).
+        /// </summary>
+        /// <param name="caPaKeyPart1">Het eerste deel van de CaPaKey van het perceel.</param>
+        /// <param name="caPaKeyPart2">Het tweede deel van de CaPaKey van het perceel.</param>
+        /// <param name="actionContextAccessor"></param>
+        /// <param name="ifNoneMatch">If-None-Match header met ETag van een vorig verzoek (optioneel). </param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Als het perceel gevonden is.</response>
+        /// <response code="403">Als u niet beschikt over de correcte rechten om deze actie uit te voeren.</response>
+        /// <response code="404">Als het perceel niet gevonden kan worden.</response>
+        /// <response code="406">Als het gevraagde formaat niet beschikbaar is.</response>
+        /// <response code="410">Als het perceel verwijderd is.</response>
+        /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+        /// <response code="500">Als er een interne fout is opgetreden.</response>
+        [HttpGet("percelen/{caPaKeyPart1}/{caPaKeyPart2}", Name = nameof(GetParcelV2CaPaKey))]
+        [ApiOrder(ApiOrder.Parcel.V2 + 2)]
+        [ProducesResponseType(typeof(ParcelDetailOsloResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status410Gone)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de response.")]
+        [SwaggerResponseHeader(StatusCodes.Status200OK, "x-correlation-id", "string", "Correlatie identificator van de response.")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(ParcelOsloResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status403Forbidden, typeof(ForbiddenResponseExamplesV2))]
+        [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(ParcelNotFoundResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status410Gone, typeof(ParcelGoneResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamplesV2))]
+        [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamplesV2))]
+        [HttpCacheExpiration(MaxAge = DefaultDetailCaching)]
+        public async Task<IActionResult> GetParcelV2CaPaKey(
+            [FromRoute] string caPaKeyPart1,
+            [FromRoute] string caPaKeyPart2,
+            [FromServices] IActionContextAccessor actionContextAccessor,
+            [FromHeader(Name = HeaderNames.IfNoneMatch)] string ifNoneMatch,
+            CancellationToken cancellationToken = default)
+        {
+            var objectId = $"{caPaKeyPart1}-{caPaKeyPart2}";
             var contentFormat = DetermineFormat(actionContextAccessor.ActionContext);
 
             RestRequest BackendRequest() => CreateBackendDetailRequest(objectId);
