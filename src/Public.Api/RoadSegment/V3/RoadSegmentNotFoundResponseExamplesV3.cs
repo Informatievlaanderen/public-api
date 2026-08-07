0d2530b0d7d25a6943aa10f4bdd32f81db3f8b85
Rik De Peuter <rikdepeuter@gmail.com>
2026-08-07 09:07:52 +0200

    feat(road): roadsegment edit geometry v3

---
+++ src/Public.Api/RoadSegment/V3/RoadSegmentNotFoundResponseExamplesV3.cs
@@ -0,0 +1,14 @@
+﻿namespace Public.Api.RoadSegment.V3;
+
+using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+using Microsoft.AspNetCore.Http;
+using RoadRegistry.BackOffice.Api.RoadSegments.V1;
+
+public class RoadSegmentNotFoundResponseExamplesV3 : RoadSegmentNotFoundResponseExamples
+{
+    public RoadSegmentNotFoundResponseExamplesV3(
+        IHttpContextAccessor httpContextAccessor,
+        ProblemDetailsHelper problemDetailsHelper)
+        : base(httpContextAccessor, problemDetailsHelper, "v3")
+    { }
+}
