de01ac790d5b2b6443e9d293e4a9e378e0977ea8
Arne Dumarey <dumarey.arne@gmail.com>
2026-09-23 13:21:02 +0200

    feat: add authorize scopes streetname

--- src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-Remove.cs
+++ src/Public.Api/StreetName/BackOffice/StreetNameBackOfficerController-Remove.cs
@@ -3,6 +3,7 @@ namespace Public.Api.StreetName.BackOffice
     using System.Threading;
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.Auth.AcmIdm;
     using Common.FeatureToggles;
     using Common.Infrastructure;
     using Common.Infrastructure.Extensions;
@@ -58,7 +59,10 @@ namespace Public.Api.StreetName.BackOffice
         [SwaggerResponseExample(StatusCodes.Status412PreconditionFailed, typeof(PreconditionFailedResponseExamplesV2))]
         [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamplesV2))]
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamplesV2))]
-        [SwaggerOperation(Description = "De straatnaam wordt verwijderd uit het straatnaamregister. Gekoppelde adressen met status `voorgesteld`, `inGebruik`, `afgekeurd` en `gehistoreerd` worden mee verwijderd. Als er een gebouweenheid of perceel gekoppeld is aan het adres, wordt deze koppeling ook verwijderd.")]
+        [SwaggerAuthorizeOperation(
+            Description = "De straatnaam wordt verwijderd uit het straatnaamregister. Gekoppelde adressen met status `voorgesteld`, `inGebruik`, `afgekeurd` en `gehistoreerd` worden mee verwijderd. Als er een gebouweenheid of perceel gekoppeld is aan het adres, wordt deze koppeling ook verwijderd.",
+            Authorize = [$"{Scopes.DvArAdresBeheer}` en `{Scopes.DvArAdresUitzonderingen}"]
+        )]
         [HttpPost(RemoveStreetNameRoute, Name = nameof(RemoveStreetName))]
         public async Task<IActionResult> RemoveStreetName(
             [FromRoute] int objectId,
