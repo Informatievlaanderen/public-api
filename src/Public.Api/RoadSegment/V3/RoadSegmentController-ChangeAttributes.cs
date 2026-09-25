fe6f4f0cf1c44822e74a19bc7d3f78a7664374ad
Arne Dumarey <dumarey.arne@gmail.com>
2026-09-25 15:25:07 +0200

    docs: align texts for road edit endpoints v3

--- src/Public.Api/RoadSegment/V3/RoadSegmentController-ChangeAttributes.cs
+++ src/Public.Api/RoadSegment/V3/RoadSegmentController-ChangeAttributes.cs
@@ -27,17 +27,21 @@ namespace Public.Api.RoadSegment.V3
         /// <param name="problemDetailsHelper"></param>
         /// <param name="featureToggle"></param>
         /// <param name="cancellationToken"></param>
-        /// <response code="202">Als het wegsegment gevonden is.</response>
+        /// <response code="202">Als het ticket succesvol is aangemaakt.</response>
         /// <response code="400">Als uw verzoek foutieve data bevat.</response>
+        /// <response code="401">Als u niet geauthenticeerd bent om deze actie uit te voeren.</response>
+        /// <response code="403">Als u niet beschikt over de correcte rechten om deze actie uit te voeren.</response>
         /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         [HttpPost(ChangeAttributesRoadSegmentRoute, Name = nameof(ChangeRoadSegmentAttributesV3))]
         [ApiOrder(ApiOrder.Road.RoadSegment.ChangeAttributes)]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
         [ProducesResponseType(typeof(Be.Vlaanderen.Basisregisters.BasicApiProblem.ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", JsonSchemaType.String, "De ETag van de response.")]
+        [SwaggerResponseHeader(StatusCodes.Status202Accepted, "location", JsonSchemaType.String, "De URL van het aangemaakte ticket.")]
         [SwaggerResponseHeader(StatusCodes.Status202Accepted, "x-correlation-id", JsonSchemaType.String, "Correlatie identificator van de response.")]
         [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamplesV3))]
         [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamplesV3))]
